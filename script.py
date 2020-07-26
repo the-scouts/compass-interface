fc0475785d59c0776ee18ae5d2877990cc6a2ea1
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-07-26 23:36:10 +0100

    Add compassread2.php recipe

--- script.py
+++ script.py
@@ -95,6 +95,29 @@ def get_report(logon: CompassLogon):
     # All works, but no control over what exported, currently using default settings.
 
 
+def compass_read(auth_keys):
+    c_logon = CompassLogon(auth_keys)
+    people = CompassPeople(c_logon.session)
+
+    member_number = c_logon.cn
+    # member_number = 760357
+
+    training_data = people._training_tab(member_number)
+    permits_data = people._permits_tab(member_number)
+    roles_detail = {role: people._scraper.get_roles_detail(role) for role in training_data["roles"]}
+
+    obj = {
+        # **training_data,
+        "roles": training_data["roles"],
+        "plps": training_data["plps"],
+        "mandatory": training_data["mandatory"],
+        "permits": permits_data,
+        "hierarchies": {role_id: detail["hierarchy"] for role_id, detail in roles_detail.items()},
+    }
+
+    return obj
+
+
 if __name__ == '__main__':
     auth_keys = ['user', 'pass']
     compass_role_to_use = 'Regional Administrator'
