a951e4be9c24a637cc470c1664f7cfaa715e86f3
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2020-07-28 03:04:57 +0100

    Streamline getting reports

--- script.py
+++ script.py
@@ -54,10 +54,8 @@ def get_report(logon: CompassLogon):
     }
 
     # GET Report Page
-    # POST Report Data (Get First View)
-    # POST Location Update (ReportViewer1$ctl04$ctl05)
-    # POST View Report (ReportViewer1$ctl04$ctl00)
-    # POST Report Data (Get Updated View)
+    # POST Location Update
+    # GET CSV data
 
     run_report_url = get_report_token(logon, reports["Appointments Report"])
 
@@ -69,54 +67,24 @@ def get_report(logon: CompassLogon):
     tree = html.fromstring(report_page.content)
     form: html.FormElement = tree.forms[0]
 
-    _scriptManagerID = "_S1"  # From ._initialize
-    _postBackSettings = {"asyncTarget": "ReportViewer1$ctl09$Reserved_AsyncLoadTarget"}
-
-    postback_form_values = {
-        _scriptManagerID: f"{_scriptManagerID}|{_postBackSettings['asyncTarget']}",
-        "__EVENTTARGET": _postBackSettings['asyncTarget'],
-        "__EVENTARGUMENT": None,
-        "__LASTFOCUS": None,
-        "__ASYNCPOST": True,
-    }
-    elements = {el.name: el.value for el in form.inputs if el.get("type") not in {'checkbox', 'image'}}
-
-    form_data = {**postback_form_values, **elements}
-    headers = {"X-MicrosoftAjax": "Delta=true", "Cache-Control": "no-cache"}
-    report = logon.post(run_report, data=form_data, headers=headers)
-    report.raise_for_status()
-    print(report.text[:75])
-
-    target = "ReportViewer1$ctl04$ctl05"
-    form_data["_S1"] = f"_S1|{target}"
-    form_data["__EVENTTARGET"] = target
+    form_data = {el.name: el.value for el in form.inputs if el.get("type") not in {'checkbox', 'image'}}
     form_data["ReportViewer1$ctl04$ctl05$txtValue"] = "Regional Roles"
     form_data["ReportViewer1$ctl04$ctl05$divDropDown$ctl01$HiddenIndices"] = "0"
-    report2 = logon.post(run_report, data=form_data, headers=headers)
-    report2.raise_for_status()
-    print(report2.text[:75])
 
-    target = "ReportViewer1$ctl04$ctl00"
-    form_data["_S1"] = f"_S1|{target}"
-    form_data["__EVENTTARGET"] = None
-    report3 = logon.post(run_report, data=form_data, headers=headers)
-    report3.raise_for_status()
-    print(report3.text[:75])
+    # Including MSFTAJAX: Delta=true reduces size by ~1kb but increases time by 0.01s.
+    # In reality we don't care about the output of this POST, just that it doesn't fail
+    report = logon.post(run_report, data=form_data, headers={"X-MicrosoftAjax": "Delta=true"})
+    report.raise_for_status()
 
-    target = _postBackSettings['asyncTarget']
-    form_data["_S1"] = f"_S1|{target}"
-    form_data["__EVENTTARGET"] = target
-    report4 = logon.post(run_report, data=form_data, headers=headers)
-    report4.raise_for_status()
-    print(report4.text[:75])
+    if "compass.scouts.org.uk%2fError.aspx|" in report.text:
+        raise requests.HTTPError("Compass Error!")
 
     print('Exporting report')
-    report_export_url_prefix, report_export_url_data = get_report_export_url(report_page.text)
-    report_csv_content = logon.get(f"{CompassSettings.base_url}/{report_export_url_prefix}", params=report_export_url_data)
-    # report_csv_content = logon.get(f"{CompassSettings.base_url}{report_export_url}CSV")  # Janky string manipulation
+    export_url_path, export_url_params = get_report_export_url(report_page.text)
+    csv_export = logon.get(f"{CompassSettings.base_url}/{export_url_path}", params=export_url_params)
     print('Saving report')
-    Path("export_report all 2.csv").write_bytes(report_csv_content.content)
-    print(len(report_csv_content.content))
+    Path("export_report all 2.csv").write_bytes(csv_export.content)
+    print(len(csv_export.content))
     print('Report Saved')
     print()
 
