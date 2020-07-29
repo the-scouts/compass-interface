1d20d426e365ea4c45568d555af1d3a32183f982
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-07-29 03:17:58 +0100

    Move compass_read to interface.py

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ from src.utility import CompassSettings
 from src.utility import jk_hash
 from typing import Tuple
 import unicodedata
+from src.interface import compass_read
 
 
 def get_report_token(logon: CompassLogon, report_number: int):
@@ -117,26 +118,6 @@ def get_report(logon: CompassLogon):
     print('Report Saved')
     print()
 
-
-def compass_read(auth: list or tuple):
-    logon = CompassLogon(auth)
-    scraper = CompassPeopleScraper(logon.session)
-
-    member_number = logon.cn
-    training_data = scraper.get_training_tab(member_number)
-    permits_data = scraper.get_permits_tab(member_number)
-    roles_detail = {role: scraper.get_roles_detail(role) for role in training_data["roles"]}
-
-    obj = {
-        # **training_data,
-        "roles": training_data["roles"],
-        "plps": training_data["plps"],
-        "mandatory": training_data["mandatory"],
-        "permits": permits_data,
-        "hierarchies": {role_id: detail["hierarchy"] for role_id, detail in roles_detail.items()},
-    }
-
-    return obj
 
 
 if __name__ == '__main__':
