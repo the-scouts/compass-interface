1cebefd8949b32a3552485f3e21d15f46033e8f4
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2020-07-29 03:17:58 +0100

    Move compass_read to interface.py

---
+++ src/interface.py
@@ -0,0 +1,24 @@
+from compass_logon import CompassLogon
+from compass_people import CompassPeopleScraper
+
+
+def compass_read(auth: list or tuple):
+    logon = CompassLogon(auth)
+    scraper = CompassPeopleScraper(logon.session)
+
+    member_number = logon.cn
+    training_data = scraper.get_training_tab(member_number)
+    permits_data = scraper.get_permits_tab(member_number)
+    roles_detail = {role: scraper.get_roles_detail(role) for role in training_data["roles"]}
+
+    obj = {
+        # **training_data,
+        "roles": training_data["roles"],
+        "plps": training_data["plps"],
+        "mandatory": training_data["mandatory"],
+        "permits": permits_data,
+        "hierarchies": {role_id: detail["hierarchy"] for role_id, detail in roles_detail.items()},
+    }
+
+    return obj
+
