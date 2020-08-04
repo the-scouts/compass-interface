54af7d27c7781c481d6d77603afdff6f615a701d
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-08-04 03:20:03 +0100

    ✨ Add authentication against Compass to the API!
    - requires a redis server running locally to store serverside state
    - Session length is currently arbitrary, and longer than Compass' 10 minutes

--- src/api/app.py
+++ src/api/app.py
@@ -2,6 +2,8 @@ import uvicorn
 from fastapi import FastAPI, APIRouter
 
 from src.api.routes import members
+from src.api.routes import authentication
+from src.api.plugins import redis
 
 app = FastAPI()
 
@@ -14,12 +16,33 @@ version_one.include_router(
     dependencies=[],
     responses={404: {"description": "Not found!"}}
 )
+version_one.include_router(
+    authentication.router,
+    responses={404: {"description": "Not found!"}}
+)
 
 app.include_router(
     version_one,
     prefix="/v1",
     dependencies=[]
 )
+
+
+class RedisConfig(redis.RedisSettings):
+    pass
+
+
+redis_plugin = redis.RedisPlugin(app, config=RedisConfig())
+
+
+@app.on_event('startup')
+async def on_startup() -> None:
+    await redis_plugin.init()
+
+
+@app.on_event('shutdown')
+async def on_shutdown() -> None:
+    await redis_plugin.terminate()
 
 # Appointments report - role details (except M01Ex, M04), ongoing details (except M01Ex imputing), disclosures
 # Member Directory - emergency contact details
