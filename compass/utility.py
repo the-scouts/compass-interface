57ee5945daf01b77e24a6ce8765e24539f996897
Rob G <rob@robg.co.uk>
2020-11-14 19:32:05 +0000

    Resolved TLS cert issue
    Compass doesn't provide the intermediate signing cert required. Added functionality to fetch and include this.

--- compass/utility.py
+++ compass/utility.py
@@ -2,8 +2,40 @@ import ast
 import ctypes
 import functools
 import threading
+import certifi
+import requests
 
 from pathlib import Path
+
+
+def setup_tls_certs():
+    """
+    Compass currently (as of 14/11/20) doesn't pass the Intermediate certificate it uses.
+    This is at time of writing the 'Thawte RSA CA 2018', which is in turned signed by DigiCert Global Root CA.
+
+    This function includes the Thawte CA cert in the Certifi chain to allow certificate verification to pass.
+
+    Yes, it's horrid. TSA plz fix.
+    """
+
+    thawteCACertURL = "https://thawte.tbs-certificats.com/Thawte_RSA_CA_2018.crt"
+
+    certifi_path = Path(certifi.where())
+    certifi_contents = certifi_path.read_text("UTF-8")
+
+    #Check for contents of Thawte CA, if not add
+    if "Thawte RSA CA 2018" not in certifi_contents:
+
+        print("Intermediate Certificate for Compass not found - Installing")
+
+        #Fetch Thawte CA from known URL, rather than including PEM
+        ca_request = requests.get(thawteCACertURL, allow_redirects=False)
+        ca_content = ca_request.text
+
+        with certifi_path.open("a", encoding="utf-8") as f:
+            f.write('\n# Label: "Thawte RSA CA 2018"\n')
+            f.write(ca_content)
+
 
 # https://stackoverflow.com/a/8831937
 def hash_code(text: str) -> int:
