a4d9a9da41258a64690b35c828365a5fc74b9c24
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-01-13 21:31:02 +0000

    🔊 Add logging formatter

--- compass/logging.py
+++ compass/logging.py
@@ -7,4 +7,9 @@ def _add_debug_handler() -> None:
     import sys
 
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
+    formatter = logging.Formatter(fmt="{asctime}.{msecs:03.0f} ({levelname}): {message}", datefmt="%Y-%m-%d %H:%M:%S", style="{")
+    console = logging.StreamHandler(stream=sys.stdout)
+    console.setFormatter(formatter)
+
+    logger.addHandler(console)
