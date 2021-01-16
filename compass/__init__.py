f13e995a03f4d3b1d9d5b1a5395d44b82f25386d
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-01-16 05:04:45 +0000

    🎨 Expose modules publicly

--- compass/__init__.py
+++ compass/__init__.py
@@ -1,5 +1,14 @@
 # This directory is a Python package.
+from compass.errors import CompassAuthenticationError
+from compass.errors import CompassError
+import compass.hierarchy
 import compass.logon
+import compass.people
+import compass.reports
+import compass.schemas.hierarchy  # TODO what schemas are should/need to be public?
+import compass.schemas.member  # TODO what schemas are should/need to be public?
+import compass.settings
+import compass.utility
 
 
 def logon(username: str, password: str, compass_role: str = None) -> compass.logon.Logon:
