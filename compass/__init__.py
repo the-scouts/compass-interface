17bf0fd9e149d4089575b7f398532b94f1923930
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-01-16 04:51:49 +0000

    ✨ Add log in convenience method

--- compass/__init__.py
+++ compass/__init__.py
@@ -1,1 +1,10 @@
 # This directory is a Python package.
+import compass.logon
+
+
+def logon(username: str, password: str, compass_role: str = None) -> compass.logon.Logon:
+    """Log in to compass, return a compass.logon.Logon object
+
+    This function is provided as a convenient interface to the logon module.
+    """
+    return compass.logon.Logon((username, password), compass_role)
