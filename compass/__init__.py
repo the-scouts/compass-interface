93e4fc92985b32ff5b3771691a7508195e1dc5ab
AA Turner <9087854+aa-turner@users.noreply.github.com>
2021-01-22 03:11:01 +0000

    🚨 Define __all__ for pylint

--- compass/__init__.py
+++ compass/__init__.py
@@ -3,17 +3,32 @@ from typing import Optional
 
 from compass.errors import CompassAuthenticationError
 from compass.errors import CompassError
-import compass.hierarchy
+from compass import hierarchy
 from compass.hierarchy import Hierarchy
-import compass.logging
+from compass import logging
 from compass.logon import Logon
-import compass.people
+from compass import people
 from compass.people import People
-import compass.reports
-import compass.schemas.hierarchy  # TODO what schemas are should/need to be public?
-import compass.schemas.member  # TODO what schemas are should/need to be public?
-import compass.settings
-import compass.utility
+from compass import reports
+from compass import schemas  # TODO what schemas are should/need to be public?
+from compass import settings
+from compass import utility
+
+__all__ = (
+    "CompassAuthenticationError",
+    "CompassError",
+    "hierarchy",
+    "Hierarchy",
+    "logging",
+    "Logon",
+    "logon",
+    "people",
+    "People",
+    "reports",
+    "schemas",
+    "settings",
+    "utility",
+)
 
 
 def logon(username: str, password: str, compass_role: Optional[str] = None) -> Logon:
