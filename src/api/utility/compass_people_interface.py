6611d385453f809a58c4c170bdea6d3cbb1a8745
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2021-01-27 00:19:27 +0000

    👽 Use CI Core - compass_people_interface.py

--- src/api/utility/compass_people_interface.py
+++ src/api/utility/compass_people_interface.py
@@ -1,14 +1,13 @@
 import datetime
 
-from src.compass.logon import CompassLogon
-from src.compass.people import CompassPeopleScraper
+import compass as ci
 
 date_zero = datetime.date(1900, 1, 1)
 ogl_names_map = {"GDPR Training": "gdpr"}
 
 
-def get_ongoing_learning_scraper(logon: CompassLogon) -> dict:
-    people_scraper = CompassPeopleScraper(logon.session)
+def get_ongoing_learning_scraper(logon: ci.Logon) -> dict:
+    people_scraper = ci.People(logon)._scraper
     ongoing = people_scraper.get_training_tab(logon.cn, ongoing_only=True)
 
     ongoing_learning = {"membership_number": logon.cn}
