ed57858afa509acd90d06327127e060b9b230aeb
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2021-02-03 23:52:45 +0000

    ✅ Tests for compass.core.interface_base

---
+++ tests/test_compass_core_interface_base.py
@@ -0,0 +1,62 @@
+from pytest import MonkeyPatch
+import requests
+
+from compass.core.interface_base import InterfaceBase
+from compass.core.settings import Settings
+
+
+class TestInterfaceBase:
+    def test_get(self, monkeypatch: MonkeyPatch):
+        # Given
+        data = "https://example.org"
+        reqs = Settings.total_requests
+
+        # When
+        s = requests.Session()
+        monkeypatch.setattr(s, "get", lambda *args, **kwargs: dict(args=args, kwargs=kwargs))
+        result = InterfaceBase(s)._get(data)
+
+        # Then
+        assert result["args"][0] == data
+        assert reqs + 1 == Settings.total_requests
+
+    def test_post(self, monkeypatch: MonkeyPatch):
+        # Given
+        data = "https://example.org"
+        json = {"abc": 123, "xyz": 321}
+        reqs = Settings.total_requests
+
+        # When
+        s = requests.Session()
+        monkeypatch.setattr(s, "post", lambda *args, **kwargs: dict(args=args, kwargs=kwargs))
+        result = InterfaceBase(s)._post(data, json=json)
+
+        # Then
+        assert result["args"][0] == data
+        assert result["kwargs"]["json"] == json
+        assert reqs + 1 == Settings.total_requests
+
+    def test_head(self, monkeypatch: MonkeyPatch):
+        # Given
+        data = "https://example.org"
+        reqs = Settings.total_requests
+
+        # When
+        s = requests.Session()
+        monkeypatch.setattr(s, "head", lambda *args, **kwargs: dict(args=args, kwargs=kwargs))
+        result = InterfaceBase(s)._head(data)
+
+        # Then
+        assert result["args"][0] == data
+        assert reqs + 1 == Settings.total_requests
+
+    def test_update_headers(self):
+        # Given
+        data = {"header_one": 123, "abc_xyz": 321}
+
+        # When
+        ib = InterfaceBase(requests.Session())
+        ib._update_headers(data)
+
+        # Then
+        assert data.items() <= ib.s.headers.items()
