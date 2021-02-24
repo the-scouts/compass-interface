d0f6e845906d22b591dd7fe4f6990b0691199510
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-02-24 20:01:47 +0000

    🚚 Move __init__.py to compass.core

--- compass/core/__init__.py
+++ compass/core/__init__.py
@@ -2,9 +2,7 @@
 
 This module exposes the public api for CI core
 """
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
+from typing import Optional
 
 from compass.core import errors
 from compass.core import logger
@@ -13,16 +11,8 @@ from compass.core.logon import Logon
 from compass.core.people import People
 from compass.core.reports import Reports
 
-if TYPE_CHECKING:
-    from typing import Optional
-
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # NoQA
-# https://stackoverflow.com/a/53486554
-# We want to expose names on the global namespace, but also use namespace packages.
-
 __all__ = (
     # sub-packages: CI-Core
-    "core",
     "errors",
     "logger",
     # public classes
@@ -33,15 +23,6 @@ __all__ = (
     # public functions
     "login",
 )
-
-# Further abuse of the import system - here we register other subpackages if they exist
-for m in ("api", "util", "interface"):
-    try:
-        __import__(f"compass.{m}")
-        __all__ += (m,)
-    except ModuleNotFoundError:
-        pass
-del m
 
 
 def login(username: str, password: str, /, *, role: Optional[str] = None, location: Optional[str] = None) -> Logon:
