cf7189ea1b25505b07ad252e5a1622246f26b89a
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-02-25 20:29:32 +0000

    🎨 Pythonise names

--- compass/core/schemas/logon.py
+++ compass/core/schemas/logon.py
@@ -5,88 +5,90 @@ from typing import Literal, Optional
 
 import pydantic
 
+TYPES_CRUD_STRING = pydantic.constr(regex=r"^[CRUD]{0,4}$")
+
 
 class CompassPropsNav(pydantic.BaseModel):
-    Action: Optional[Literal["None"]] = None
-    StartNo: Optional[int] = None
-    StartPage: Optional[int] = None
+    action: Optional[Literal["None"]] = pydantic.Field(None, alias="Action")
+    start_no: Optional[int] = pydantic.Field(None, alias="StartNo")
+    start_page: Optional[int] = pydantic.Field(None, alias="StartPage")
 
 
 class CompassPropsPage(pydantic.BaseModel):
-    UseCN: int
-    HideBadges: Optional[bool] = None
-    Croc: Optional[Literal["OK"]] = None
-    HideNominations: Optional[bool] = None
-    CanDeleteOGLHrs: Optional[bool] = None
-    FoldName: Optional[Literal["MP_"]] = None
+    use_cn: Optional[int] = pydantic.Field(None, alias="UseCN")
+    hide_badges: Optional[bool] = pydantic.Field(None, alias="HideBadges")
+    croc: Optional[Literal["OK"]] = pydantic.Field(None, alias="Croc")
+    hide_nominations: Optional[bool] = pydantic.Field(None, alias="HideNominations")
+    can_delete_ogl_hrs: Optional[bool] = pydantic.Field(None, alias="CanDeleteOGLHrs")
+    fold_name: Optional[Literal["MP_"]] = pydantic.Field(None, alias="FoldName")
 
 
 class CompassPropsCRUD(pydantic.BaseModel):
-    MDIS: Optional[str] = None
-    ROLES: Optional[str] = None
-    PEMD: Optional[str] = None
-    MMMD: Optional[str] = None
-    MVID: Optional[str] = None
-    PERM: Optional[str] = None
-    TRN: Optional[pydantic.constr(regex=r"^[CRUD]{0,4}$")] = None
+    mdis: Optional[TYPES_CRUD_STRING] = pydantic.Field(None, alias="MDIS")
+    roles: Optional[TYPES_CRUD_STRING] = pydantic.Field(None, alias="ROLES")
+    pemd: Optional[TYPES_CRUD_STRING] = pydantic.Field(None, alias="PEMD")
+    mmmd: Optional[TYPES_CRUD_STRING] = pydantic.Field(None, alias="MMMD")
+    mvid: Optional[TYPES_CRUD_STRING] = pydantic.Field(None, alias="MVID")
+    perm: Optional[TYPES_CRUD_STRING] = pydantic.Field(None, alias="PERM")
+    trn: Optional[TYPES_CRUD_STRING] = pydantic.Field(None, alias="TRN")
 
 
 class CompassPropsUser(pydantic.BaseModel):
-    IsMe: Optional[bool] = None
+    is_me: Optional[bool] = pydantic.Field(None, alias="IsMe")
 
 
 class CompassPropsMasterSSO(pydantic.BaseModel):
-    ON: Optional[int] = None
+    on: Optional[int] = pydantic.Field(None, alias="ON")
 
 
 class CompassPropsMasterUser(pydantic.BaseModel):
-    CN: Optional[int] = None
-    MRN: Optional[int] = None
-    ON: Optional[int] = None
-    LVL: Optional[Literal["ORG", "ORST", "CNTR", "CNST", "REG", "RGST", "CNTY", "CTST", "DIST", "DTST", "SGRP", "SGST"]] = None
-    JK: Optional[str] = None  # SHA2-512
+    cn: Optional[int] = pydantic.Field(None, alias="CN")
+    mrn: Optional[int] = pydantic.Field(None, alias="MRN")
+    on: Optional[int] = pydantic.Field(None, alias="ON")
+    lvl: Optional[Literal["ORG", "ORST", "CNTR", "CNST", "REG", "RGST", "CNTY", "CTST", "DIST", "DTST", "SGRP", "SGST"]] = pydantic.Field(None, alias="LVL")
+    jk: Optional[str] = pydantic.Field(None, alias="JK")
 
 
 class CompassPropsMasterConst(pydantic.BaseModel):
-    Wales: Optional[int] = 10000007
-    Scotland: Optional[int] = 10000005
-    OverSeas: Optional[int] = 10000002
-    HQ: Optional[int] = 10000001
+    wales: Optional[int] = pydantic.Field(None, alias="Wales")
+    scotland: Optional[int] = pydantic.Field(None, alias="Scotland")
+    over_seas: Optional[int] = pydantic.Field(None, alias="OverSeas")
+    hq: Optional[int] = pydantic.Field(None, alias="HQ")
 
 
 class CompassPropsMasterSys(pydantic.BaseModel):
-    SessionID: Optional[str] = None
-    SafeJSON: Optional[bool] = None
-    WebPath: Optional[pydantic.HttpUrl] = None
-    TextSize: Optional[int] = None
-    Timout: Optional[int] = None
-    REST: Optional[bool] = None
-    HardTime: Optional[datetime.time] = None
-    HardExpiry: Optional[str] = None
-    TimeoutExtension: Optional[int] = None
-    Ping: Optional[int] = None
+    session_id: Optional[str] = pydantic.Field(None, alias="SessionID")
+    safe_json: Optional[bool] = pydantic.Field(None, alias="SafeJSON")
+    web_path: Optional[pydantic.HttpUrl] = pydantic.Field(None, alias="WebPath")
+    text_size: Optional[int] = pydantic.Field(None, alias="TextSize")
+    timout: Optional[int] = pydantic.Field(None, alias="Timout")
+    rest: Optional[bool] = pydantic.Field(None, alias="REST")
+    hard_time: Optional[datetime.time] = pydantic.Field(None, alias="HardTime")
+    hard_expiry: Optional[str] = pydantic.Field(None, alias="HardExpiry")
+    timeout_extension: Optional[int] = pydantic.Field(None, alias="TimeoutExtension")
+    ping: Optional[int] = pydantic.Field(None, alias="Ping")
 
 
 class CompassPropsMaster(pydantic.BaseModel):
-    SSO: CompassPropsMasterSSO
-    User: CompassPropsMasterUser
-    Const: CompassPropsMasterConst
-    Sys: CompassPropsMasterSys
+    sso: CompassPropsMasterSSO = pydantic.Field(..., alias="SSO")
+    user: CompassPropsMasterUser = pydantic.Field(..., alias="User")
+    const: CompassPropsMasterConst = pydantic.Field(..., alias="Const")
+    sys: CompassPropsMasterSys = pydantic.Field(..., alias="Sys")
 
 
 class CompassPropsConstSys(pydantic.BaseModel):
-    STO: Optional[int] = None
-    STO_ASK: Optional[int] = None
+    sto: Optional[int] = pydantic.Field(None, alias="STO")
+    sto_ask: Optional[int] = pydantic.Field(None, alias="STO_ASK")
 
 
 class CompassPropsConst(pydantic.BaseModel):
-    Sys: CompassPropsConstSys
+    sys: CompassPropsConstSys = pydantic.Field(..., alias="Sys")
 
 
 class CompassProps(pydantic.BaseModel):
-    Nav: CompassPropsNav
-    Page: CompassPropsPage
-    CRUD: CompassPropsCRUD
-    User: CompassPropsUser
-    Master: CompassPropsMaster
-    Const: CompassPropsConst
+    nav: CompassPropsNav = pydantic.Field(..., alias="Nav")
+    page: CompassPropsPage = pydantic.Field(..., alias="Page")
+    crud: CompassPropsCRUD = pydantic.Field(..., alias="CRUD")
+    user: CompassPropsUser = pydantic.Field(..., alias="User")
+    master: CompassPropsMaster = pydantic.Field(..., alias="Master")
+    const: CompassPropsConst = pydantic.Field(..., alias="Const")
