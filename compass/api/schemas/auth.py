14e85a993e8bf2a81e137d59ba375e1295193f17
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-02-26 01:09:33 +0000

    ✨ Cut out login overhead, reuse existing sessions

--- compass/api/schemas/auth.py
+++ compass/api/schemas/auth.py
@@ -1,13 +1,17 @@
+import datetime
 from typing import Literal, Optional
 
 import pydantic
 
-from compass.core.schemas.member import MemberBase
+from compass.core.schemas.logon import CompassPropsMasterUser
 
 
-class User(MemberBase):
+class User(pydantic.BaseModel):
     selected_role: tuple[str, str]
     logon_info: tuple[str, str, Optional[str], Optional[str]]
+    asp_net_id: str
+    props: CompassPropsMasterUser
+    expires: datetime.datetime
 
 
 class Token(pydantic.BaseModel):
