8b3bde50593afbdb6de646a6bc819da5b8f06d79
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-02-27 05:15:15 +0000

    ✨ Use pydantic BaseSettings

--- compass/core/settings.py
+++ compass/core/settings.py
@@ -1,8 +1,16 @@
-class Settings:
-    base_domain = "compass.scouts.org.uk"
-    base_url = f"https://{base_domain}"
-    date_format = "%d %B %Y"  # dd Month YYYY
-    org_number = 10000001
-    total_requests = 0
-    wcf_json_endpoint = "/JSon.svc"  # Windows communication foundation JSON service endpoint
-    web_service_path = base_url + wcf_json_endpoint
+import pydantic
+
+
+class Settings(pydantic.BaseSettings):
+    base_domain: str = "compass.scouts.org.uk"
+    base_url: pydantic.HttpUrl = f"https://{base_domain}"
+    date_format: str = "%d %B %Y"  # dd Month YYYY
+    org_number: int = 10000001
+    total_requests: int = 0
+    wcf_json_endpoint: str = "/JSon.svc"  # Windows communication foundation JSON service endpoint
+    web_service_path: pydantic.HttpUrl = base_url + wcf_json_endpoint
+    debug: bool = False
+
+    class Config:  # noqa: D106
+        case_sensitive = False  # this is the default, but mark for clarity.
+        env_prefix = "CI_"  # env variables named `REDIS_HOST` etc
