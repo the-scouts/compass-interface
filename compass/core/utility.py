da446ce094adead0216ef7d6e83cf6e341f1edee
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-03-18 02:09:10 +0000

    ✨ Implement generic auth_header_get, jk_hash!

--- compass/core/utility.py
+++ compass/core/utility.py
@@ -5,9 +5,11 @@ import ctypes
 import datetime
 import functools
 import threading
+import time
 from typing import Any, Optional, TYPE_CHECKING
 
 import pydantic
+import requests
 
 from compass.core.logger import logger
 from compass.core.settings import Settings
@@ -92,6 +94,78 @@ class PeriodicTimer:
         self.thread.cancel()
         return self
 
+
+def jk_hash(session: requests.Session, membership_number: int, role_number: int, jk: str) -> str:
+    """Generate JK Hash needed by Compass."""
+    # hash_code(f"{time.time() * 1000:.0f}")
+    key_hash = f"{time.time() * 1000:.0f}{jk}{role_number}{membership_number}"  # JK, MRN & CN are all required.
+    data = compass_restify({"pKeyHash": key_hash, "pCN": membership_number})
+    logger.debug(f"Sending preflight data {datetime.datetime.now()}")
+    session.post(f"{Settings.base_url}/System/Preflight", json=data)
+    Settings.total_requests += 1
+    return key_hash
+
+
+def auth_header_get(
+    membership_number: int,
+    role_number: int,
+    jk: str,
+    session: requests.Session,
+    url: str,
+    params: Optional[dict[str, Optional[str]]] = None,
+    headers: Optional[dict[str, str]] = None,
+    stream: Optional[bool] = None,
+    **kwargs: Any
+) -> requests.Response:
+    """Sends a HTTP GET request.
+
+    Pass-through method to requests.sessions.Session.get, also adding to
+    the counter of total requests sent by `compass.core`.
+
+    Adds custom auth_header logic for certain Compass requests
+    See Scouts.js -> $.ajaxSetup -> beforeSend for details (links below)
+    https://github.com/the-scouts/compass-interface/blob/master/js/Scouts.js#L1122
+    https://github.com/the-scouts/compass-interface/blob/master/js/Scouts.js#L1128-L1129
+
+    Logic in source comments is auth_header logic is only added for **AJAX**
+    GET calls matching the following logic:
+
+    if method == "GET":
+        if compass_props.master.sys.safe_json is True and "system/preflight" not in url:
+            return True
+        elif url.lower().replace(Settings.web_service_path.lower(), "").startswith("sto_check")
+            return False
+        return True
+
+    Args:
+        membership_number: Current authenticated user's membership number
+        role_number: Current authenticated user's active role number
+        jk: Current authenticated user's ??? (Ideas: Join Key??? SHA2-512)
+        session: Active session, initialised by compass.core.Logon
+        url: Request URL
+        params: Mapping to be sent in the query string for the request
+        headers: Mapping of HTTP Headers
+        stream: Whether to stream download the response content.
+        kwargs: Optional arguments to requests.sessions.Session.get
+
+    Returns:
+        requests.Response object from executing the request
+
+    Raises:
+        requests.exceptions.RequestException
+
+    """
+    # pylint: disable=too-many-arguments
+    # pylint complains that we have more than 5 arguments.
+    headers = (headers or {}) | {"Auth": jk_hash(session, membership_number, role_number, jk)}
+
+    params = (params or {}) | {
+        "x1": f"{membership_number}",
+        "x2": f"{jk}",
+        "x3": f"{role_number}",
+    }
+
+    return session.get(url, params=params, headers=headers, stream=stream, **kwargs)
 
 #
 # def set_interval(interval: int):
