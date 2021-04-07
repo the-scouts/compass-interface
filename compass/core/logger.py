7ccbb113ef0e516ef8288bb2f1a6d93236991ff0
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-07 03:25:58 +0100

    ✨ Enable optionally logging to a file

--- compass/core/logger.py
+++ compass/core/logger.py
@@ -1,23 +1,30 @@
+from __future__ import annotations
+
 import logging
+from typing import Optional, TYPE_CHECKING
 
 from compass.core.settings import Settings
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 logger = logging.getLogger("compass_interface")
 
 
-def enable_debug_logging() -> None:
-    import sys  # pylint: disable=import-outside-toplevel
-
-    # Only want to import sys if needed
+def enable_debug_logging(*, log_file: Optional[Path] = None) -> None:
+    import sys  # pylint: disable=import-outside-toplevel (Only want to import sys if needed)
 
     logger.setLevel(logging.DEBUG)
-
     formatter = logging.Formatter(fmt="{asctime}.{msecs:03.0f} ({levelname}): {message}", datefmt="%Y-%m-%d %H:%M:%S", style="{")
-    console = logging.StreamHandler(stream=sys.stdout)
-    console.setFormatter(formatter)
 
-    logger.addHandler(console)
+    handlers = [logging.StreamHandler(stream=sys.stdout)]  # always include a default console handler
+    if log_file is not None:
+        handlers.append(logging.FileHandler(log_file.with_suffix(".log"), encoding="utf-8"))
+
+    for handler in handlers:
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
 
 if Settings.debug:
-    enable_debug_logging()
+    enable_debug_logging(log_file=Settings.log_file)
