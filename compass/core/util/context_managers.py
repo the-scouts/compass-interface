da62bd367cecbfda58864ebb6447b4581eec8fb7
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-07 15:24:28 +0100

    🐛 Always need to yield something, so yield None

--- compass/core/util/context_managers.py
+++ compass/core/util/context_managers.py
@@ -34,15 +34,17 @@ def validation_errors_logging(id_value: int, name: str = "Member No") -> Iterato
 
 @contextlib.contextmanager
 def get_cached_json(filename: Path, /, *, expected_type: type = type(None)):
+    if Settings.cache_to_file is False:
+        yield None
+        return  # don't process the rest of this context manager
     try:
-        if Settings.cache_to_file is True:
-            # Attempt to see if the data has been fetched already and is on the local system
-            json_data = json.loads(filename.read_text(encoding="UTF8"))
-            if json_data:
-                if type(None) != expected_type and isinstance(json_data, expected_type):
-                    yield json_data
-                else:
-                    yield json_data
+        # Attempt to see if the data has been fetched already and is on the local system
+        json_data = json.loads(filename.read_text(encoding="UTF8"))
+        if json_data:
+            if type(None) != expected_type and isinstance(json_data, expected_type):
+                yield json_data
+            else:
+                yield json_data
     except FileNotFoundError:
         # Otherwise run the function
         yield
