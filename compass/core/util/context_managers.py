fbd61f7c8f033cd15ff4905221a2632c78c1df01
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-07 16:02:03 +0100

    🐛 Fix logic, and simplify default expected type

--- compass/core/util/context_managers.py
+++ compass/core/util/context_managers.py
@@ -33,7 +33,7 @@ def validation_errors_logging(id_value: int, name: str = "Member No") -> Iterato
 
 
 @contextlib.contextmanager
-def get_cached_json(filename: Path, /, *, expected_type: type = type(None)):
+def get_cached_json(filename: Path, /, *, expected_type: type = object):
     if Settings.cache_to_file is False:
         yield None
         return  # don't process the rest of this context manager
@@ -41,8 +41,11 @@ def get_cached_json(filename: Path, /, *, expected_type: type = type(None)):
         # Attempt to see if the data has been fetched already and is on the local system
         json_data = json.loads(filename.read_text(encoding="UTF8"))
         if json_data:
-            if type(None) != expected_type and isinstance(json_data, expected_type):
-                yield json_data
+            if object != expected_type:
+                if isinstance(json_data, expected_type):
+                    yield json_data
+                else:
+                    yield None
             else:
                 yield json_data
     except FileNotFoundError:
