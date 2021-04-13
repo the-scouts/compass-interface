85bef01b335310a01b2d0fecbe93ddd04b8403d8
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-13 20:19:07 +0100

    🎨 Use time_cache

--- compass/core/util/time_cache.py
+++ compass/core/util/time_cache.py
@@ -1,22 +1,25 @@
+from __future__ import annotations
+
 import time
 from typing import TypeVar
 
-T = TypeVar('T', bound=object)
+T = TypeVar("T", bound=object)
 _cache: dict[tuple[str, int], T] = {}
 
 
-def set_key(key: tuple[str, int], value: T) -> T:
-    _cache[key] = time.gmtime()[:5], value
+def set_key(key: tuple[str, int], /, value: T) -> T:
+    _cache[key] = time.gmtime(), value
     return value
 
 
-def get_key(item: tuple[str, int]) -> T | None:
-    if item in _cache:
-        time_stored, value = _cache[item]
-        if time.time() - time.mktime(time_stored + (0, 0, 0, 0)) < 60 * 60 * 1:
-            return value
-        del _cache[item]
-    return None
+def get_key(key_type: str, key_id: int, /) -> T | None:
+    pair = _cache.get((key_type, key_id))
+    if pair is None:
+        return None
+    time_stored, value = pair
+    if time.time() - time.mktime(time_stored + (0, 0, 0, 0)) < 60 * 60 * 1:
+        return value
+    del _cache[key_type, key_id]
 
 
 clear = _cache.clear()
