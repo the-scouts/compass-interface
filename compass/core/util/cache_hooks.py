43c0ceecf15c458ce8cfc97b16c7394c3c5e0230
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-18 01:52:39 +0100

    🔥 Move cache implementation to `compass.util.cache`

--- compass/core/util/cache_hooks.py
+++ compass/core/util/cache_hooks.py
@@ -1,16 +1,11 @@
 from __future__ import annotations
 
 import functools
-import json
-from pathlib import Path
-import time
 from typing import Literal, TYPE_CHECKING, TypeVar
 
 import pydantic
-from pydantic.json import pydantic_encoder
 
 from compass.core.settings import Settings
-from compass.core.util import context_managers
 
 if TYPE_CHECKING:
     from collections.abc import Callable
@@ -20,8 +15,6 @@ if TYPE_CHECKING:
     RT = TypeVar("RT")  # Return Type
     C = Callable[..., RT]
 
-_cache: dict[tuple[str, int], tuple[int, object]] = {}
-
 
 class _Opt:  # avoid global scope
     BACKEND_DISK: bool = False
@@ -29,49 +22,6 @@ class _Opt:  # avoid global scope
     set_cache: Callable[[tuple[str, int], T], T] = lambda key, value: value  # default no-op
     get_cache: Callable[[tuple[str, int]], T | None] = lambda key: None  # default no-op
     clear_cache: Callable[[], None] = lambda: None  # default no-op
-
-
-def set_val(key: tuple[str, int], value: T, /) -> T:
-    if _Opt.BACKEND_DISK:
-        key_type, key_id = key
-        filename = Path(f"cache/{key_type}-{key_id}.json")
-        with context_managers.filesystem_guard(f"Unable to write cache file to {filename}"):
-            filename.write_text(json.dumps(value, ensure_ascii=False, default=pydantic_encoder), encoding="utf-8")
-    else:
-        _cache[key] = time.monotonic_ns() // 10 ** 9, value
-    return value
-
-
-def get_val(key: tuple[str, int]) -> T | None:
-    if _Opt.BACKEND_DISK:
-        key_type, key_id = key
-        filename = Path(f"cache/{key_type}-{key_id}.json")
-        if not filename.is_file():  # catching errors is expensive, so check first
-            return None
-        try:
-            json_data: object = json.loads(filename.read_text(encoding="utf-8"))
-            if json_data and time.time() - filename.stat().st_mtime < _Opt.EXPIRY_SECONDS:
-                return json_data  # type: ignore[return-value]
-        except FileNotFoundError:
-            return None
-    else:
-        if key is None:
-            raise ValueError("Key is None!")
-        pair = _cache.get(key)
-        if pair is None:
-            return None
-        time_stored, value = pair
-        if time.monotonic() - time_stored < _Opt.EXPIRY_SECONDS:
-            return value  # type: ignore[return-value]
-        del _cache[key]
-        return None
-
-
-def clear() -> None:
-    if _Opt.BACKEND_DISK:
-        pass  # TODO clear cache/ directory
-    else:
-        _cache.clear()
 
 
 def setup_cache(
