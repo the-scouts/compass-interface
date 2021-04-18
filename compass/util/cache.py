b03478621c8d6e0c67c4319d5fd12d7582c2abec
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-18 03:44:38 +0100

    🚚 Update backend option processing

--- compass/util/cache.py
+++ compass/util/cache.py
@@ -8,21 +8,21 @@ from typing import Literal, TYPE_CHECKING, TypeVar
 from pydantic.json import pydantic_encoder
 
 from compass.core.util import context_managers
-from compass.core.util.cache_hooks import _Opt
-from compass.core.util.cache_hooks import setup_cache as setup_cache_hooks
+from compass.core.util.cache_hooks import setup_cache_hooks
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
-
     T = TypeVar("T", bound=object)
-    RT = TypeVar("RT")  # Return Type
-    C = Callable[..., RT]
 
 _cache: dict[tuple[str, int], tuple[int, object]] = {}
 
 
+class _BackendOpt:
+    BACKEND_DISK: bool
+    EXPIRY_SECONDS: int
+
+
 def set_val(key: tuple[str, int], value: T, /) -> T:
-    if _Opt.BACKEND_DISK:
+    if _BackendOpt.BACKEND_DISK:
         key_type, key_id = key
         filename = Path(f"cache/{key_type}-{key_id}.json")
         with context_managers.filesystem_guard(f"Unable to write cache file to {filename}"):
@@ -33,14 +33,14 @@ def set_val(key: tuple[str, int], value: T, /) -> T:
 
 
 def get_val(key: tuple[str, int]) -> T | None:
-    if _Opt.BACKEND_DISK:
+    if _BackendOpt.BACKEND_DISK:
         key_type, key_id = key
         filename = Path(f"cache/{key_type}-{key_id}.json")
         if not filename.is_file():  # catching errors is expensive, so check first
             return None
         try:
             json_data: object = json.loads(filename.read_text(encoding="utf-8"))
-            if json_data and time.time() - filename.stat().st_mtime < _Opt.EXPIRY_SECONDS:
+            if json_data and time.time() - filename.stat().st_mtime < _BackendOpt.EXPIRY_SECONDS:
                 return json_data  # type: ignore[return-value]
         except FileNotFoundError:
             return None
@@ -51,17 +51,10 @@ def get_val(key: tuple[str, int]) -> T | None:
         if pair is None:
             return None
         time_stored, value = pair
-        if time.monotonic() - time_stored < _Opt.EXPIRY_SECONDS:
+        if time.monotonic() - time_stored < _BackendOpt.EXPIRY_SECONDS:
             return value  # type: ignore[return-value]
         del _cache[key]
         return None
-
-
-def clear() -> None:
-    if _Opt.BACKEND_DISK:
-        pass  # TODO clear cache/ directory
-    else:
-        _cache.clear()
 
 
 def setup_cache(backend: Literal["memory", "disk"], expiry: int = 0):
@@ -72,4 +65,6 @@ def setup_cache(backend: Literal["memory", "disk"], expiry: int = 0):
         expiry: Cache expiry in minutes. 0 to disable time-based expiry
 
     """
-    setup_cache_hooks(backend, set_val, get_val, clear, expiry)
+    setup_cache_hooks(set_val, get_val, expiry == 0)
+    _BackendOpt.BACKEND_DISK = backend == "disk"
+    _BackendOpt.EXPIRY_SECONDS = expiry * 60
