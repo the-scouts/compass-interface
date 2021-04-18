83696e3238cb91f1c670a2cd378cfac51723dbb1
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-18 03:34:35 +0100

    🏷️ Yet more work on type safety

--- compass/core/util/cache_hooks.py
+++ compass/core/util/cache_hooks.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import functools
-from typing import Literal, TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, TypeVar, TypedDict
 
 import pydantic
 
@@ -12,23 +12,29 @@ if TYPE_CHECKING:
     from collections.abc import Hashable
 
     T = TypeVar("T", bound=object)
-    RT = TypeVar("RT")  # Return Type
-    C = Callable[..., RT]
+    C = Callable[..., T]
 
 
-class _Opt:  # avoid global scope
-    BACKEND_DISK: bool = False
-    EXPIRY_SECONDS: int = 60
-    set_cache: Callable[[tuple[str, int], T], T] = lambda key, value: value  # default no-op
-    get_cache: Callable[[tuple[str, int]], T | None] = lambda key: None  # default no-op
-    clear_cache: Callable[[], None] = lambda: None  # default no-op
+def default_set(_key: tuple[str, int | Hashable], value: T, /) -> T:  # default no-op
+    return value
+
+
+def default_get(_key: tuple[str, int | Hashable]) -> T | None:  # default no-op
+    return None
+
+
+class _OptClass(TypedDict):  # avoid global scope
+    EXPIRY_SECONDS: int
+    set_cache: Callable[[tuple[str, int | Hashable], T], T]
+    get_cache: Callable[[tuple[str, int | Hashable]], T | None]
+
+
+_Opt = _OptClass(EXPIRY_SECONDS=3_600, set_cache=default_set, get_cache=default_get)
 
 
 def setup_cache(
-    set_cache: Callable[[tuple[str, int], T], T],
-    get_cache: Callable[[tuple[str, int]], T | None],
-    clear_cache: Callable[[], None],
-    backend: Literal["memory", "disk"],
+    set_cache: Callable[[tuple[str, int | Hashable], T], T],
+    get_cache: Callable[[tuple[str, int | Hashable]], T | None],
     expiry: int = 0,
 ) -> None:
     """Turn on caching and set options.
@@ -36,39 +42,33 @@ def setup_cache(
     Args:
         set_cache: Function to set value to given key
         get_cache: Function to retrieve value from a given key
-        clear_cache: Function to clear the cache
-        backend: Cache to disk or in-memory
         expiry: Cache expiry in minutes. 0 to disable time-based expiry
 
     """
     Settings.use_cache = True
-    _Opt.BACKEND_DISK = backend == "disk"
-    _Opt.EXPIRY_SECONDS = expiry * 60
-    _Opt.set_cache = set_cache
-    _Opt.get_cache = get_cache
-    _Opt.clear_cache = clear_cache
+    _Opt["EXPIRY_SECONDS"] = expiry * 60
+    _Opt["set_cache"] = set_cache
+    _Opt["get_cache"] = get_cache
 
 
-def cache_result(*, key, model_type: type[RT] | None = None) -> Callable[[C], C]:
-    def decorating_function(user_function: C) -> C:
-        wrapper = _cache_wrapper(user_function, key, model_type)
-        return functools.update_wrapper(wrapper, user_function)
+def cache_result(*, key: tuple[str, int], model_type: type | None = None) -> Callable[[C[T]], C[T]]:
+    def decorating_function(user_function: C[T]) -> C[T]:
+        return _cache_wrapper(user_function, key, model_type)
     return decorating_function
 
 
-def _cache_wrapper(user_function: C, key: tuple[str, int], model_type: type[RT] | None = None) -> C:
-    if False and Settings.use_cache is False or _Opt.EXPIRY_SECONDS == 0:  # No caching
+def _cache_wrapper(user_function: C[T], key: tuple[str, int], model_type: type | None = None) -> C[T]:
+    if False and Settings.use_cache is False or _Opt["EXPIRY_SECONDS"] == 0:  # No caching
         return user_function
     else:
         def wrapper(*args: Hashable, **kwargs: Hashable) -> T:
             key_ = key[0], (*args, *kwargs)[key[1]]  # second arg is position of arg
-            result = _Opt.get_cache(key_)
+            result = _Opt["get_cache"](key_)
             if result is not None:
                 if model_type is not None:
                     return pydantic.parse_obj_as(model_type, result)
                 return result
             result = user_function(*args, **kwargs)
-            _Opt.set_cache(key_, result)
+            _Opt["set_cache"](key_, result)
             return result
-    wrapper.clear_cache = _Opt.clear_cache
-    return wrapper
+        return functools.update_wrapper(wrapper, user_function)
