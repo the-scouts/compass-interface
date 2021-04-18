e63289829e311fbfbffc0d55fce7160aad09a0a5
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-18 03:38:23 +0100

    🎨 Use `disable_cache` for cache backends

--- compass/core/util/cache_hooks.py
+++ compass/core/util/cache_hooks.py
@@ -24,29 +24,29 @@ def default_get(_key: tuple[str, int | Hashable]) -> T | None:  # default no-op
 
 
 class _OptClass(TypedDict):  # avoid global scope
-    EXPIRY_SECONDS: int
+    disable_cache: bool
     set_cache: Callable[[tuple[str, int | Hashable], T], T]
     get_cache: Callable[[tuple[str, int | Hashable]], T | None]
 
 
-_Opt = _OptClass(EXPIRY_SECONDS=3_600, set_cache=default_set, get_cache=default_get)
+_Opt = _OptClass(disable_cache=True, set_cache=default_set, get_cache=default_get)
 
 
 def setup_cache(
     set_cache: Callable[[tuple[str, int | Hashable], T], T],
     get_cache: Callable[[tuple[str, int | Hashable]], T | None],
-    expiry: int = 0,
+    disable_cache: bool = False,
 ) -> None:
     """Turn on caching and set options.
 
     Args:
         set_cache: Function to set value to given key
         get_cache: Function to retrieve value from a given key
-        expiry: Cache expiry in minutes. 0 to disable time-based expiry
+        disable_cache: Local disable of cache, for usage by backends
 
     """
     Settings.use_cache = True
-    _Opt["EXPIRY_SECONDS"] = expiry * 60
+    _Opt["disable_cache"] = disable_cache
     _Opt["set_cache"] = set_cache
     _Opt["get_cache"] = get_cache
 
@@ -58,11 +58,11 @@ def cache_result(*, key: tuple[str, int], model_type: type | None = None) -> Cal
 
 
 def _cache_wrapper(user_function: C[T], key: tuple[str, int], model_type: type | None = None) -> C[T]:
-    if False and Settings.use_cache is False or _Opt["EXPIRY_SECONDS"] == 0:  # No caching
+    if Settings.use_cache is False or _Opt["disable_cache"] == 0:  # No caching
         return user_function
     else:
         def wrapper(*args: Hashable, **kwargs: Hashable) -> T:
-            key_ = key[0], (*args, *kwargs)[key[1]]  # second arg is position of arg
+            key_ = key[0], (*args, *kwargs)[key[1]]  # second tuple item is position of key_id in function args
             result = _Opt["get_cache"](key_)
             if result is not None:
                 if model_type is not None:
