2387a1f35d03fd00452a9f8a934b08643c12899e
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-25 07:31:41 +0100

    🔖 RLS: v0.22.8

--- compass/api/app.py
+++ compass/api/app.py
@@ -33,7 +33,7 @@ and is licensed under the **[MIT license](https://choosealicense.com/licenses/mi
 app = FastAPI(
     title="Compass Interface — the unofficial Compass API",
     description=long_description,
-    version="0.22.7",
+    version="0.22.8",
     openapi_tags=open_api_tag_metadata,
 )
 app.router.lifespan_context = redis.lifetime
