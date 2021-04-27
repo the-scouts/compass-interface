d606ec6da57197cb4de8dfdd756aea7d6b72fabe
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-27 04:01:20 +0100

    🐛 Return validated value!

--- compass/core/util/auth_header.py
+++ compass/core/util/auth_header.py
@@ -13,16 +13,16 @@ if TYPE_CHECKING:
 
     import requests
 
-    from compass.core.util import client
+    from compass.core.util.client import Client
 
 
-def jk_hash(session: client.Client, membership_number: int, role_number: int, jk: str) -> str:
+def jk_hash(client: Client, membership_number: int, role_number: int, jk: str) -> str:
     """Generate JK Hash needed by Compass."""
     # hash_code(f"{time.time() * 1000:.0f}")
     key_hash = f"{time.time() * 1000:.0f}{jk}{role_number}{membership_number}"  # JK, MRN & CN are all required.
     data = compass_restify({"pKeyHash": key_hash, "pCN": membership_number})
     logger.debug(f"Sending preflight data {datetime.datetime.now()}")
-    session.post(f"{Settings.base_url}/System/Preflight", json=data)
+    client.post(f"{Settings.base_url}/System/Preflight", json=data)
     Settings.total_requests += 1
     return key_hash
 
@@ -31,7 +31,7 @@ def auth_header_get(
     membership_number: int,
     role_number: int,
     jk: str,
-    session: client.Client,
+    client: Client,
     url: str,
     *,
     params: Optional[Mapping[str, Optional[str]]] = None,
@@ -63,7 +63,7 @@ def auth_header_get(
         membership_number: Current authenticated user's membership number
         role_number: Current authenticated user's active role number
         jk: Current authenticated user's ??? (Ideas: Join Key??? SHA2-512)
-        session: Active session, initialised by compass.core.Logon
+        client: HTTP client
         url: Request URL
         params: Mapping to be sent in the query string for the request
         headers: Mapping of HTTP Headers
@@ -79,7 +79,7 @@ def auth_header_get(
     """
     # pylint: disable=too-many-arguments
     # pylint complains that we have more than 5 arguments.
-    headers = dict(headers or {}) | {"Auth": jk_hash(session, membership_number, role_number, jk)}
+    headers = dict(headers or {}) | {"Auth": jk_hash(client, membership_number, role_number, jk)}
 
     params = dict(params or {}) | {
         "x1": f"{membership_number}",
@@ -87,4 +87,4 @@ def auth_header_get(
         "x3": f"{role_number}",
     }
 
-    return session.get(url, params=params, headers=headers, stream=stream, **kwargs)
+    return client.get(url, params=params, headers=headers, stream=stream, **kwargs)
