be3d1ba5438ff4c0808951fa5670f5e0ffbeca73
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-30 04:31:51 +0100

    🎨 Add OAuth details model

--- compass/api/schemas/auth.py
+++ compass/api/schemas/auth.py
@@ -1,9 +1,25 @@
-import datetime
 from typing import Literal, Optional
 
+from fastapi import params
 import pydantic
 
 from compass.core.schemas.logon import CompassPropsMasterUser
+
+
+class OAuth2Details(pydantic.BaseModel):
+    grant_type: Literal["password"] = "password"
+    username: str
+    password: str
+    scope: str = ""  # e.g. "items:read items:write users:read profile openid"
+    # not part of OAuth2 spec:
+    role: Optional[str]
+    location: Optional[str]
+
+
+class Form(params.Form):
+    def __init__(self, default, **kwargs):
+        super().__init__(default, **kwargs)
+        self.embed = False
 
 
 class User(pydantic.BaseModel):
@@ -16,4 +32,4 @@ class User(pydantic.BaseModel):
 
 class Token(pydantic.BaseModel):
     access_token: str
-    token_type: Literal["bearer"]
+    token_type: Literal["bearer"] = "bearer"
