71bc4b2a64e103724abd091bf62a75e83899a96e
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-30 05:57:46 +0100

    📝 Route docstrings

--- compass/api/routes/members.py
+++ compass/api/routes/members.py
@@ -37,6 +37,7 @@ class ErrorHandling(contextlib.AbstractAsyncContextManager):
 
 @router.get("/me", response_model=member.MemberDetails)
 async def get_current_member(people: ci.People = Depends(people_accessor)) -> member.MemberDetails:
+    """Gets my personal details."""
     logger.debug(f"Getting /me for {people.membership_number}")
     async with ErrorHandling():
         return people.personal(people.membership_number)
@@ -44,6 +45,7 @@ async def get_current_member(people: ci.People = Depends(people_accessor)) -> me
 
 @router.get("/me/roles", response_model=member.MemberRolesCollection)
 async def get_current_member_roles(people: ci.People = Depends(people_accessor), volunteer_only: bool = False) -> member.MemberRolesCollection:
+    """Gets my roles."""
     logger.debug(f"Getting /me/roles for {people.membership_number}")
     async with ErrorHandling():
         return people.roles(people.membership_number, only_volunteer_roles=volunteer_only)
@@ -51,6 +53,7 @@ async def get_current_member_roles(people: ci.People = Depends(people_accessor),
 
 @router.get("/me/permits", response_model=list[member.MemberPermit])
 async def get_current_member_permits(people: ci.People = Depends(people_accessor)) -> list[member.MemberPermit]:
+    """Gets my permits."""
     logger.debug(f"Getting /me/permits for {people.membership_number}")
     async with ErrorHandling():
         return people.permits(people.membership_number)
@@ -58,6 +61,7 @@ async def get_current_member_permits(people: ci.People = Depends(people_accessor
 
 @router.get("/me/training", response_model=member.MemberTrainingTab)
 async def get_current_member_training(people: ci.People = Depends(people_accessor)) -> member.MemberTrainingTab:
+    """Gets my training."""
     logger.debug(f"Getting /me/training for {people.membership_number}")
     async with ErrorHandling():
         return people.training(people.membership_number)
@@ -65,6 +69,7 @@ async def get_current_member_training(people: ci.People = Depends(people_accesso
 
 @router.get("/me/ongoing-learning", response_model=member.MemberMandatoryTraining)
 async def get_current_member_ongoing_learning(people: ci.People = Depends(people_accessor)) -> member.MemberMandatoryTraining:
+    """Gets my ongoing learning."""
     logger.debug(f"Getting /me/ongoing for {people.membership_number}")
     async with ErrorHandling():
         return people.ongoing_learning(people.membership_number)
@@ -72,6 +77,7 @@ async def get_current_member_ongoing_learning(people: ci.People = Depends(people
 
 @router.get("/me/awards", response_model=list[member.MemberAward])
 async def get_current_member_awards(people: ci.People = Depends(people_accessor)) -> list[member.MemberAward]:
+    """Gets my awards."""
     logger.debug(f"Getting /me/awards for {people.membership_number}")
     async with ErrorHandling():
         return people.awards(people.membership_number)
@@ -79,6 +85,7 @@ async def get_current_member_awards(people: ci.People = Depends(people_accessor)
 
 @router.get("/me/disclosures", response_model=list[member.MemberDisclosure])
 async def get_current_member_disclosures(people: ci.People = Depends(people_accessor)) -> list[member.MemberDisclosure]:
+    """Gets my disclosures."""
     logger.debug(f"Getting /me/disclosures for {people.membership_number}")
     async with ErrorHandling():
         return people.disclosures(people.membership_number)
@@ -86,6 +93,7 @@ async def get_current_member_disclosures(people: ci.People = Depends(people_acce
 
 @router.get("/me/latest-disclosure", response_model=Optional[member.MemberDisclosure])
 async def get_current_member_latest_disclosure(people: ci.People = Depends(people_accessor)) -> Optional[member.MemberDisclosure]:
+    """Gets my latest disclosure."""
     logger.debug(f"Getting /me/latest-disclosure for {people.membership_number}")
     async with ErrorHandling():
         return people.latest_disclosure(people.membership_number)
@@ -93,12 +101,7 @@ async def get_current_member_latest_disclosure(people: ci.People = Depends(peopl
 
 @router.get("/{compass_id}", response_model=member.MemberDetails)
 async def get_member(compass_id: int, people: ci.People = Depends(people_accessor)) -> member.MemberDetails:
-    """Gets profile details for given member
-
-    Args:
-        compass_id: Member to retrieve information for
-        people: People object
-    """
+    """Gets personal details for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}} for {people.membership_number}")
     async with ErrorHandling():
         return people.personal(compass_id)
@@ -106,6 +109,7 @@ async def get_member(compass_id: int, people: ci.People = Depends(people_accesso
 
 @router.get("/{compass_id}/roles", response_model=member.MemberRolesCollection)
 async def get_member_roles(compass_id: int, people: ci.People = Depends(people_accessor)):
+    """Gets roles for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}}/roles for {people.membership_number}")
     async with ErrorHandling():
         return people.roles(compass_id, only_volunteer_roles=False)
@@ -113,6 +117,7 @@ async def get_member_roles(compass_id: int, people: ci.People = Depends(people_a
 
 @router.get("/{compass_id}/permits", response_model=list[member.MemberPermit])
 async def get_member_permits(compass_id: int, people: ci.People = Depends(people_accessor)) -> list[member.MemberPermit]:
+    """Gets permits for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}}/permits for {people.membership_number}")
     async with ErrorHandling():
         return people.permits(compass_id)
@@ -120,6 +125,7 @@ async def get_member_permits(compass_id: int, people: ci.People = Depends(people
 
 @router.get("/{compass_id}/training", response_model=member.MemberTrainingTab)
 async def get_current_member_training(compass_id: int, people: ci.People = Depends(people_accessor)) -> member.MemberTrainingTab:
+    """Gets training for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}}/training for {people.membership_number}")
     async with ErrorHandling():
         return people.training(compass_id)
@@ -127,6 +133,7 @@ async def get_current_member_training(compass_id: int, people: ci.People = Depen
 
 @router.get("/{compass_id}/ongoing-learning", response_model=member.MemberMandatoryTraining)
 async def get_ongoing_learning(compass_id: int, people: ci.People = Depends(people_accessor)) -> member.MemberMandatoryTraining:
+    """Gets ongoing learning for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}}/ongoing-learning for {people.membership_number}")
     async with ErrorHandling():
         return people.ongoing_learning(compass_id)
@@ -134,6 +141,7 @@ async def get_ongoing_learning(compass_id: int, people: ci.People = Depends(peop
 
 @router.get("/{compass_id}/awards", response_model=list[member.MemberAward])
 async def get_current_member_awards(compass_id: int, people: ci.People = Depends(people_accessor)) -> list[member.MemberAward]:
+    """Gets awards for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}}/awards for {people.membership_number}")
     async with ErrorHandling():
         return people.awards(compass_id)
@@ -141,6 +149,7 @@ async def get_current_member_awards(compass_id: int, people: ci.People = Depends
 
 @router.get("/{compass_id}/disclosures", response_model=list[member.MemberDisclosure])
 async def get_current_member_disclosures(compass_id: int, people: ci.People = Depends(people_accessor)) -> list[member.MemberDisclosure]:
+    """Gets disclosures for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}}/disclosures for {people.membership_number}")
     async with ErrorHandling():
         return people.disclosures(compass_id)
@@ -148,6 +157,7 @@ async def get_current_member_disclosures(compass_id: int, people: ci.People = De
 
 @router.get("/{compass_id}/latest-disclosure", response_model=Optional[member.MemberDisclosure])
 async def get_current_member_latest_disclosure(compass_id: int, people: ci.People = Depends(people_accessor)) -> member.MemberDisclosure:
+    """Gets the latest disclosure for the member given by `compass_id`."""
     logger.debug(f"Getting /{{compass_id}}/latest-disclosure for {people.membership_number}")
     async with ErrorHandling():
         return people.latest_disclosure(compass_id)
