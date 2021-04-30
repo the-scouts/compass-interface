5d37c9ca6e3df3b1d6d041033572f3395f3153bc
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-30 04:32:46 +0100

    🎨 Update token route to use defined model

--- compass/api/routes/authentication.py
+++ compass/api/routes/authentication.py
@@ -1,25 +1,23 @@
 from aioredis import Redis
-from fastapi import APIRouter
-from fastapi import Depends
+import fastapi
 from fastapi import HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 
 from compass.api.plugins.redis import depends_redis
-from compass.api.schemas.auth import Token
-from compass.api.utility.oauth2 import login_token_store_session
+from compass.api.schemas import auth
+from compass.api.utility.oauth2 import create_token
 from compass.core.logger import logger
 
-router = APIRouter()
+router = fastapi.APIRouter()
 
 
-@router.post("/", response_model=Token)
-async def login_for_access_token(store: Redis = Depends(depends_redis), form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
+@router.post("/", response_model=auth.Token)
+async def login_for_access_token(store: Redis = fastapi.Depends(depends_redis), form_data: auth.OAuth2Details = auth.Form(...)) -> auth.Token:
     logger.debug(f"Requested token endpoint with form data: {form_data.__dict__}")
     try:
-        access_token = await login_token_store_session(form_data.username, form_data.password, None, None, store=store)
-    except Exception as err:
-        if isinstance(err, HTTPException):
-            raise err
+        access_token = await create_token(form_data.username, form_data.password, form_data.role, form_data.location, store)
+    except HTTPException as err:
+        raise err
+    except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Authentication error!") from None
-    return Token(access_token=access_token, token_type="bearer")
+    return auth.Token(access_token=access_token)
