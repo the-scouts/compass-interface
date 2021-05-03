12a7d240858fa9a1a8f53b19b99551edd0b3eeec
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-05-03 18:51:27 +0100

    🔧 Update black config after new release

--- compass/api/routes/authentication.py
+++ compass/api/routes/authentication.py
@@ -12,10 +12,13 @@ router = fastapi.APIRouter()
 
 
 @router.post("/", response_model=auth.Token)
-async def login_for_access_token(store: Redis = fastapi.Depends(get_redis), form_data: auth.OAuth2Details = auth.Form(...)) -> auth.Token:
+async def login_for_access_token(
+    store: Redis = fastapi.Depends(get_redis), form_data: auth.OAuth2Details = auth.Form(...)
+) -> auth.Token:
     logger.debug(f"Requested token endpoint with form data: {form_data.__dict__}")
     try:
-        access_token = await create_token(form_data.username, form_data.password, form_data.role, form_data.location, store)
+        role = "Regional Administrator"
+        access_token = await create_token(form_data.username, form_data.password, role, form_data.location, store)
     except HTTPException as err:
         raise err
     except Exception:
