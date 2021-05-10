9d6d324ae66308481934dcaeb70e19e046ced94e
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-05-10 23:26:02 +0100

    🥅 Add common error handling

---
+++ compass/api/util/errors.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import contextlib
+from types import TracebackType
+from typing import NoReturn
+
+from fastapi import HTTPException
+from starlette import status
+from starlette import exceptions
+
+from compass.core import errors
+
+
+def http_error(status_code: int, error_code: str, message: str, /, headers: dict[str, str] | None = None) -> NoReturn:
+    raise HTTPException(status_code, {"code": error_code, "message": message}, headers=headers)
+
+
+class ErrorHandling(contextlib.AbstractAsyncContextManager):
+    def __init__(self, overrides: dict[type[errors.CompassError], tuple[int, str, str]], /):
+        self.overrides = overrides
+
+    async def __aexit__(self, exc_type: type[BaseException] | None, _val: BaseException | None, _tb: TracebackType | None):
+        if not exc_type:
+            return True
+
+        if exc_type in self.overrides:
+            status_code, error_code, message = self.overrides[exc_type]
+            raise http_error(status_code, error_code, message)
+
+        # pass-through for Starlette HTTP exceptions
+        if exc_type == exceptions.HTTPException:
+            raise
+
+        # fallback
+        if exc_type == errors.CompassPermissionError:
+            raise http_error(status.HTTP_403_FORBIDDEN, "R2", "Your current role does not have permission to do this!")
+        if exc_type == errors.CompassNetworkError:
+            raise http_error(status.HTTP_503_SERVICE_UNAVAILABLE, "R1", "The request to the Compass server failed!")
+        if exc_type == errors.CompassError:
+            raise http_error(status.HTTP_500_INTERNAL_SERVER_ERROR, "Z1", "API Error (Core)! Please contact Adam.")
+        if exc_type == Exception:
+            raise http_error(status.HTTP_500_INTERNAL_SERVER_ERROR, "Z0", "Server panic (Interpreter)! Please contact Adam.")
+
