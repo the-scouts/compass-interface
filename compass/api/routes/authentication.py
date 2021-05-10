7a02106f926d455cbe4e321b7d98c03595348f44
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-05-10 23:26:53 +0100

    ♻️ Update routes for common errors

--- compass/api/routes/authentication.py
+++ compass/api/routes/authentication.py
@@ -1,26 +1,24 @@
-from aioredis import Redis
 import fastapi
-from fastapi import HTTPException
+from starlette import exceptions
 from starlette import status
+from starlette import requests
 
 from compass.api.schemas import auth
 from compass.api.util.oauth2 import create_token
-from compass.api.util.redis import get_redis
+from compass.api.util.errors import http_error
 from compass.core.logger import logger
 
 router = fastapi.APIRouter()
 
 
 @router.post("/", response_model=auth.Token)
-async def login_for_access_token(
-    store: Redis = fastapi.Depends(get_redis), form_data: auth.OAuth2Details = auth.Form(...)
-) -> auth.Token:
+async def login_for_access_token(request: requests.Request, form_data: auth.OAuth2Details = auth.Form(...)) -> auth.Token:
     logger.debug(f"Requested token endpoint with form data: {form_data.__dict__}")
     try:
-        role = "Regional Administrator"
-        access_token = await create_token(form_data.username, form_data.password, role, form_data.location, store)
-    except HTTPException as err:
+        store = request.app.state.redis
+        access_token = await create_token(form_data.username, form_data.password, form_data.role, form_data.location, store)
+    except exceptions.HTTPException as err:
         raise err
     except Exception:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "Authentication error!") from None
+        raise http_error(status.HTTP_500_INTERNAL_SERVER_ERROR, "A1", "Authentication error!") from None
     return auth.Token(access_token=access_token)
