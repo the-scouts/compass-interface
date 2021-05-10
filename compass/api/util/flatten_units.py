18ed39932d98109f541d79ff09954badfe9b4443
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-05-11 00:24:49 +0100

    ♻️ Add `unit_records.py`

--- compass/api/util/flatten_units.py
+++ compass/api/util/flatten_units.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
-from typing import NamedTuple, TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Union
 
-from compass.core.logon import Logon
-from compass.core.settings import Settings
-from compass.core.schemas import hierarchy as schema
+from compass.api.schemas.unit_records import UnitRecord
 import compass.core as ci
+from compass.core.logon import Logon
+from compass.core.schemas import hierarchy as schema
+from compass.core.settings import Settings
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
@@ -15,20 +16,6 @@ if TYPE_CHECKING:
 RESOURCE_FOLDER = Path(__file__, "../../resources").resolve()
 TREE_PATH = RESOURCE_FOLDER.joinpath("hierarchy_tree.json")
 FLAT_PATH = RESOURCE_FOLDER.joinpath("hierarchy_flat.json")
-
-
-class UnitRecord(NamedTuple):
-    name: str
-    parent: int
-    children: dict[int, str]
-    sections: dict[int, str]
-
-
-class NullableUnitRecord(NamedTuple):
-    name: str
-    parent: int
-    children: Union[dict[int, str], None]
-    sections: Union[dict[int, str], None]
 
 
 def _flatten_hierarchy(d: Union[schema.UnitData, schema.DescendantData], parent_id: int) -> Iterator[tuple[int, UnitRecord]]:
@@ -51,12 +38,12 @@ def make_hierarchy_resource(session: Logon) -> dict[int, UnitRecord]:
     return flat_hierarchy
 
 
-def load_hierarchy_map(path: Path = FLAT_PATH) -> dict[int, NullableUnitRecord]:
+def load_hierarchy_map(path: Path = FLAT_PATH) -> dict[int, UnitRecord]:
     json_hierarchy = json.loads(path.read_text(encoding="utf-8"))
     flat_hierarchy = {}
     for unit_id, unit_data in json_hierarchy.items():
         name, parent_id, children, sections = unit_data
         children = {int(unit_id): unit_name for unit_id, unit_name in children.items()} or None
         sections = {int(unit_id): unit_name for unit_id, unit_name in sections.items()} or None
-        flat_hierarchy[int(unit_id)] = NullableUnitRecord(name, parent_id, children, sections)
+        flat_hierarchy[int(unit_id)] = UnitRecord(name, parent_id, children, sections)
     return flat_hierarchy
