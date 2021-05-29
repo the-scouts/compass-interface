66bd7e2dea3b793194b9114092b4f8cc11de645f
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-05-29 04:57:14 +0100

    ⚡️ Faster date parsing

--- compass/core/util/type_coercion.py
+++ compass/core/util/type_coercion.py
@@ -1,6 +1,34 @@
 import datetime
 from typing import Any, Optional
 
+from compass.core import errors
+
+months_lookup = {
+    'january': 1,
+    'jan': 1,
+    'february': 2,
+    'feb': 2,
+    'march': 3,
+    'mar': 3,
+    'april': 4,
+    'apr': 4,
+    'may': 5,
+    'june': 6,
+    'jun': 6,
+    'july': 7,
+    'jul': 7,
+    'august': 8,
+    'aug': 8,
+    'september': 9,
+    'sep': 9,
+    'october': 10,
+    'oct': 10,
+    'november': 11,
+    'nov': 11,
+    'december': 12,
+    'dec': 12,
+}
+
 
 def maybe_int(value: Any) -> Optional[int]:
     """Casts value to int or None."""
@@ -10,10 +38,29 @@ def maybe_int(value: Any) -> Optional[int]:
         return None
 
 
-def parse_date(date_time_str: str) -> Optional[datetime.date]:
-    if not date_time_str:
+def parse_date(value: str) -> Optional[datetime.date]:
+    """Custom date parsing function
+
+    datetime.datetime.strptime works, this is 10x quicker
+
+    Two date formats:
+     - %d %B %Y  e.g. 01 January 2000
+     - %d %b %Y  e.g. 01 Jan 2000
+
+     Args:
+         value: date string to parse
+
+    Returns:
+        Parsed date object
+
+    Raises:
+        CompassError: If given string is not in a recognised date format
+
+    """
+    if not value:
         return None
     try:
-        return datetime.datetime.strptime(date_time_str, "%d %B %Y").date()  # e.g. 01 January 2000
-    except ValueError:
-        return datetime.datetime.strptime(date_time_str, "%d %b %Y").date()  # e.g. 01 Jan 2000
+        day, month, year = value.split(" ", 2)
+        return datetime.date(int(year), months_lookup[month.lower()], int(day))
+    except (KeyError, ValueError):
+        raise errors.CompassError(f"Parsing string `{value}` into a date failed!")
