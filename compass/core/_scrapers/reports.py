548129b2bebf99bcd7dab53c4811a3af11ef4a97
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-06-02 01:55:34 +0100

    🎨 Various reports updates

--- compass/core/_scrapers/reports.py
+++ compass/core/_scrapers/reports.py
@@ -7,6 +7,7 @@ import time
 from typing import Optional, TYPE_CHECKING
 import urllib.parse
 
+import requests
 from lxml import html
 
 import compass.core as ci
@@ -38,7 +39,7 @@ def get_report_token(client: Client, auth_ids: tuple[int, int, str], report_numb
 
     report_token_uri = str(response.json().get("d"))
     if report_token_uri not in {"-1", "-2", "-3", "-4"}:
-        return report_token_uri
+        return f"{Settings.base_url}/{report_token_uri}"
     if report_token_uri in {"-2", "-3"}:
         raise ci.CompassReportError("Report aborted: Report No Longer Available")
     if report_token_uri == "-4":  # nosec (false positive B105; not a hardcoded passwordstring)
@@ -47,79 +48,93 @@ def get_report_token(client: Client, auth_ids: tuple[int, int, str], report_numb
 
 
 def get_report_export_url(report_page: str, filename: Optional[str] = None) -> tuple[str, dict[str, str]]:
-    full_url_encoded = re.search(r'"ExportUrlBase":"(.*?)"', report_page).group(1)  # type: ignore[union-attr]
-    fragments = urllib.parse.urlparse(full_url_encoded.encode().decode("unicode-escape"))
-    export_url_path = fragments.path[1:]  # strip leading `/`
-    report_export_url_data = dict(urllib.parse.parse_qsl(fragments.query, keep_blank_values=True))
-    report_export_url_data["Format"] = "CSV"
-    if filename is not None:
-        report_export_url_data["FileName"] = filename
+    cut = report_page[report_page.index("ExportUrlBase"):].removeprefix('ExportUrlBase":"')
+    full_url = cut[:cut.index('"')].encode().decode("unicode-escape")
+    return f"{full_url}CSV", {}
 
-    return export_url_path, report_export_url_data
+    # fragments = urllib.parse.urlparse(full_url)
+    # export_url_path = fragments.path[1:]  # strip leading `/`
+    # report_export_url_data = dict(urllib.parse.parse_qsl(fragments.query, keep_blank_values=True))
+    # report_export_url_data["Format"] = "CSV"
+    # if filename is not None:
+    #     report_export_url_data["FileName"] = filename
+    #
+    # return export_url_path, report_export_url_data
 
 
 def get_report_page(client: Client, run_report_url: str) -> bytes:
-    # TODO what breaks if we don't update user-agent?
-    # Compass does user-agent sniffing in reports!!!
-    client.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
-
     # Get initial reports page, for export URL and config.
     logger.info("Generating report")
-    report_page = client.get(f"{Settings.base_url}/{run_report_url}")
+    report_page = client.get(run_report_url)
 
     return report_page.content
 
 
-def update_form_data(client: Client, report_page: bytes, run_report: str) -> None:
-    form = html.fromstring(report_page).forms[0]
-    elements = {el.name: el.value for el in form.inputs if el.get("type") not in {"checkbox", "image"}}
+def update_form_data(client: Client, report_page: bytes, run_report: str, full_extract: bool = True) -> None:
+    tree = html.fromstring(report_page)
+    # form_data = {"__VIEWSTATE": next((el.value for el in tree.forms[0].iter("input") if el.name == "__VIEWSTATE"), "")}
+    form_data = {el.name: el.value for el in tree.forms[0].inputs if el.get("type") not in {"checkbox", "image"}}
+    form_data |= {
+        "ReportViewer1$ctl10": "ltr",
+        "ReportViewer1$ctl11": "standards",
+        "ReportViewer1$ctl05$ctl00$CurrentPage": "1",
+        "ReportViewer1$ctl09$VisibilityState$ctl00": "ReportPage",
+        "__EVENTTARGET": "ReportViewer1$ctl04$ctl07",
+        "__EVENTARGUMENT": None,
+        "__LASTFOCUS": None,
+        "__ASYNCPOST": "true"
+    }  # TODO this may not be needed. Test.
 
     # Table Controls: table#ParametersGridReportViewer1_ctl04
     # ReportViewer1$ctl04$ctl03$ddValue - Region/County(District) Label
     # ReportViewer1$ctl04$ctl05$txtValue - County Label
     # ReportViewer1$ctl04$ctl07$txtValue - District Label
-    # ReportViewer1$ctl04$ctl09$txtValue - Role Types (Status)
+    # ReportViewer1$ctl04$ctl09$txtValue - Role Statuses
     # ReportViewer1$ctl04$ctl15$txtValue - Columns Label
 
     # ReportViewer1_ctl04_ctl07_divDropDown - Districts
     # ReportViewer1_ctl04_ctl05_divDropDown - Counties
-    # ReportViewer1_ctl04_ctl09_divDropDown - Role Types
+    # ReportViewer1_ctl04_ctl09_divDropDown - Role Statuses
     # ReportViewer1_ctl04_ctl15_divDropDown - Columns
 
-    form_data = {
-        "__VIEWSTATE": elements["__VIEWSTATE"],
-        "ReportViewer1$ctl04$ctl05$txtValue": "Regional Roles",
-        "ReportViewer1$ctl04$ctl05$divDropDown$ctl01$HiddenIndices": "0",
-    }
+    numbered_counties = _parse_drop_down_list(tree, "ReportViewer1_ctl04_ctl05_divDropDown")
+    numbered_districts = _parse_drop_down_list(tree, "ReportViewer1_ctl04_ctl07_divDropDown")
+    numbered_role_statuses = _parse_drop_down_list(tree, "ReportViewer1_ctl04_ctl09_divDropDown")
+    numbered_column_names = _parse_drop_down_list(tree, "ReportViewer1_ctl04_ctl15_divDropDown")
+    # numbered_districts = ",".join(str(i) for i in range(len(tree.get_element_by_id("ReportViewer1_ctl04_ctl07_divDropDown")[0][0])-1))
 
-    # districts = tree.xpath("//div[@id='ReportViewer1_ctl04_ctl07_divDropDown']//label/text()")
-    # list_districts = [unicodedata.normalize("NFKD", d) for d in districts if "select all" not in d.lower()]
-    # numbered_districts = {str(i): d for i, d in enumerate(list_districts)}
-    # all_districts = ", ".join(numbered_districts.values())
-    # all_districts_indices = ",".join(numbered_districts.keys())
-    #
-    # counties = tree.xpath("//div[@id='ReportViewer1_ctl04_ctl05_divDropDown']//label/text()")
-    # list_counties = [unicodedata.normalize("NFKD", c) for c in counties if "select all" not in c.lower()]
-    # numbered_counties = {str(i): c for i, c in enumerate(list_counties)}
-    # all_counties = ", ".join(numbered_counties.values())
-    # all_counties_indices = ",".join(numbered_counties.keys())
-    #
-    # form_data = {
-    #     "__VIEWSTATE": elements["__VIEWSTATE"],
-    #     "ReportViewer1$ctl04$ctl05$txtValue": all_counties,
-    #     "ReportViewer1$ctl04$ctl05$divDropDown$ctl01$HiddenIndices": all_counties_indices,
-    #     "ReportViewer1$ctl04$ctl07$txtValue": all_districts,
-    #     "ReportViewer1$ctl04$ctl07$divDropDown$ctl01$HiddenIndices": all_districts_indices,
-    # }
+    # # Export regional roles only
+    # form_data["ReportViewer1$ctl04$ctl05$txtValue"] = "Regional Roles"
+    # form_data["ReportViewer1$ctl04$ctl05$divDropDown$ctl01$HiddenIndices"] = "0"
 
-    # Including MicrosoftAJAX: Delta=true reduces size by ~1kb but increases time by 0.01s.
+    # Export all districts
+    form_data["ReportViewer1$ctl04$ctl05$txtValue"] = ", ".join(numbered_counties.values())
+    form_data["ReportViewer1$ctl04$ctl05$divDropDown$ctl01$HiddenIndices"] = ",".join(numbered_counties.keys())
+    form_data["ReportViewer1$ctl04$ctl07$txtValue"] = ", ".join(numbered_districts.values())
+    form_data["ReportViewer1$ctl04$ctl07$divDropDown$ctl01$HiddenIndices"] = ",".join(numbered_districts.keys())
+
+    # TODO this may not be needed. Test.
+    # update text values of role statuses and column names from default indices
+    form_data["ReportViewer1$ctl04$ctl09$txtValue"] = ", ".join(numbered_role_statuses[i] for i in form_data.get("ReportViewer1$ctl04$ctl09$divDropDown$ctl01$HiddenIndices", "").split(","))
+    form_data["ReportViewer1$ctl04$ctl15$txtValue"] = ", ".join(numbered_column_names[i] for i in form_data.get("ReportViewer1$ctl04$ctl15$divDropDown$ctl01$HiddenIndices", "").split(","))
+
+    # Including MicrosoftAJAX: Delta=true lets us check errors quickly
     # In reality we don't care about the output of this POST, just that it doesn't fail
-    report = client.post(run_report, data=form_data, headers={"X-MicrosoftAjax": "Delta=true"})
-    report.raise_for_status()
+    # Compass does user-agent sniffing in reports!!!
+    report = client.post(run_report, data=form_data, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)", "X-MicrosoftAjax": "Delta=true"})
 
     # Check error state
+    try:
+        report.raise_for_status()
+    except requests.HTTPError as err:
+        raise ci.CompassNetworkError("Updating report locations failed!") from err
     if "compass.scouts.org.uk%2fError.aspx|" in report.text:
         raise ci.CompassReportError("Compass Error!")
+
+
+def _parse_drop_down_list(tree: html.HtmlElement, element_id: str, /) -> dict[str, str]:
+    table = tree.get_element_by_id(element_id)[0][0]
+    return {str(i): row[0][0][0][1].text.replace("\xa0", " ") for i, row in enumerate(table[1:])}
 
 
 def report_keep_alive(client: Client, report_page: str) -> str:
@@ -142,7 +157,7 @@ def download_report_streaming(client: Client, url: str, params: dict[str, str], 
 def download_report_normal(client: Client, url: str, params: dict[str, str], filename: str) -> bytes:
     start = time.time()
     csv_export = client.get(url, params=params)
-    logger.debug(f"Exporting took {time.time() - start}s")
+    logger.debug(f"Exporting took {time.time() - start:.2f}s")
     logger.info("Saving report")
     with context_managers.filesystem_guard("Unable to write report export"):
         Path(filename).write_bytes(csv_export.content)
