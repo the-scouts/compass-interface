1050800048e7e61c200676efcd7b87e2aa2c4163
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-07 13:23:00 +0100

    ✅ Add more `Login.__init__` tests

--- tests/test_compass_core_logon.py
+++ tests/test_compass_core_logon.py
@@ -24,10 +24,140 @@ from tests.util.fake_compass import asp_net_id
 
 base_domain = "127.0.0.1"
 base_url = "http://127.0.0.1:4200"
+jk_value = "9b65d68f4aca0138b5bae4492e7cdfae220a834e3e69731d996be1ddbb496d32fd29497d4f9729525c9fbc77666bb520ea214c0802ea22b958e6ae525224fd15"
+session_id_value = "d6c76537-1b6c-3910-c3d4-d21d4e6453a6"
 
 
 class TestLogon:
-    def test_login_from_session_props(self, server):
+    def test_login_init(self):
+        # Given
+        Settings.base_url = base_url
+        client = Client()
+        client.cookies.set("ASP.NET_SessionId", asp_net_id, domain=Settings.base_domain)
+        user_props = {"cn": 10000000, "mrn": 9000000, "on": 10000001, "lvl": "ORG", "jk": jk_value}
+        session_id = session_id_value
+        compass_props = ci.CompassProps.parse_obj({"master": {"user": user_props, "sys": {"session_id": session_id}}})
+        current_role = "Role", "Place"
+
+        # When
+        session = logon.Logon(client=client, compass_props=compass_props, current_role=current_role)
+
+        # Then
+        assert isinstance(session._client, Client)
+        assert isinstance(session._client, httpx.Client)
+        assert session.compass_props == compass_props
+        assert session.current_role == current_role
+        assert isinstance(session.hierarchy, ci.HierarchyLevel)
+        assert session.hierarchy.unit_id == 10000001
+        assert session.hierarchy.level == "Organisation"
+        assert session.membership_number == 10000000
+        assert session.role_number == 9000000
+        assert session._jk == user_props["jk"]
+        assert session._asp_net_id == asp_net_id
+        assert session._session_id == session_id_value
+
+    def test_login_init_no_unit_number(self):
+        # Given
+        Settings.base_url = base_url
+        client = Client()
+        compass_props = ci.CompassProps.parse_obj({"master": {"user": {"lvl": "ORG"}}})
+        current_role = "Role", "Place"
+
+        # Then
+        with pytest.raises(ci.CompassError, match="Unit Number and Level must be specified!"):
+            # When
+            logon.Logon(client=client, compass_props=compass_props, current_role=current_role)
+
+    def test_login_init_no_unit_level(self):
+        # Given
+        Settings.base_url = base_url
+        client = Client()
+        compass_props = ci.CompassProps.parse_obj({"master": {"user": {"on": 10000001}}})
+        current_role = "Role", "Place"
+
+        # Then
+        with pytest.raises(ci.CompassError, match="Unit Number and Level must be specified!"):
+            # When
+            logon.Logon(client=client, compass_props=compass_props, current_role=current_role)
+
+    def test_login_init_no_membership_number(self):
+        # Given
+        Settings.base_url = base_url
+        client = Client()
+        user_props = {"mrn": 9000000, "on": 10000001, "lvl": "ORG", "jk": jk_value}
+        compass_props = ci.CompassProps.parse_obj({"master": {"user": user_props}})
+        current_role = "Role", "Place"
+
+        # Then
+        with pytest.raises(ci.CompassError, match="User IDs must be specified!"):
+            # When
+            logon.Logon(client=client, compass_props=compass_props, current_role=current_role)
+
+    def test_login_init_no_role_number(self):
+        # Given
+        Settings.base_url = base_url
+        client = Client()
+        user_props = {"cn": 10000000, "on": 10000001, "lvl": "ORG", "jk": jk_value}
+        compass_props = ci.CompassProps.parse_obj({"master": {"user": user_props}})
+        current_role = "Role", "Place"
+
+        # Then
+        with pytest.raises(ci.CompassError, match="User IDs must be specified!"):
+            # When
+            logon.Logon(client=client, compass_props=compass_props, current_role=current_role)
+
+    def test_login_init_no_jk(self):
+        # Given
+        Settings.base_url = base_url
+        client = Client()
+        user_props = {"cn": 10000000, "mrn": 9000000, "on": 10000001, "lvl": "ORG"}
+        compass_props = ci.CompassProps.parse_obj({"master": {"user": user_props}})
+        current_role = "Role", "Place"
+
+        # Then
+        with pytest.raises(ci.CompassError, match="User IDs must be specified!"):
+            # When
+            logon.Logon(client=client, compass_props=compass_props, current_role=current_role)
+
+    def test_login_init_no_session_id(self):
+        # Given
+        Settings.base_url = base_url
+        client = Client()
+        client.cookies.set("ASP.NET_SessionId", asp_net_id, domain=Settings.base_domain)
+        user_props = {"cn": 10000000, "mrn": 9000000, "on": 10000001, "lvl": "ORG", "jk": jk_value}
+        compass_props = ci.CompassProps.parse_obj({"master": {"user": user_props}})
+        current_role = "Role", "Place"
+
+        # Then
+        with pytest.raises(ci.CompassError, match="ASP.NET ID must be specified!"):
+            # When
+            logon.Logon(client=client, compass_props=compass_props, current_role=current_role)
+
+    def test_login_from_logon(self, server):
+        # Given
+        Settings.base_url = base_url
+        credentials = "username", "password"
+
+        # When
+        session = logon.Logon.from_logon(credentials)
+
+        # Then
+        assert session._client.cookies["ASP.NET_SessionId"] == asp_net_id
+        assert session._asp_net_id == asp_net_id
+
+        assert session._session_id == session_id_value
+        assert session._client.headers["SID"] == session_id_value
+
+        assert session._client.headers["Authorization"] == "10000000~9000000"
+
+        assert session.current_role == ("Regional Administrator", "Wessex")
+        assert session.hierarchy.unit_id == 10000001
+        assert session.hierarchy.level == "Organisation"
+        assert session.membership_number == 10000000
+        assert session.role_number == 9000000
+        assert session._jk == jk_value
+
+    def test_login_from_session(self):
         # Given
         Settings.base_url = base_url
         user_props = {
@@ -35,9 +165,9 @@ class TestLogon:
             "mrn": 9000000,
             "on": 10000001,
             "lvl": "ORG",
-            "jk": "9b65d68f4aca0138b5bae4492e7cdfae220a834e3e69731d996be1ddbb496d32fd29497d4f9729525c9fbc77666bb520ea214c0802ea22b958e6ae525224fd15",
+            "jk": jk_value,
         }
-        session_id = "d6c76537-1b6c-3910-c3d4-d21d4e6453a6"
+        session_id = session_id_value
         current_role = "Role", "Place"
 
         # When
@@ -135,11 +265,11 @@ class TestLogon:
                     mrn=9000000,
                     on=10000001,
                     lvl="ORG",
-                    jk="9b65d68f4aca0138b5bae4492e7cdfae220a834e3e69731d996be1ddbb496d32fd29497d4f9729525c9fbc77666bb520ea214c0802ea22b958e6ae525224fd15",
+                    jk=jk_value,
                 ),
                 const=CompassPropsMasterConst(wales=10000007, scotland=10000005, over_seas=10000002, hq=10000001),
                 sys=CompassPropsMasterSys(
-                    session_id="d6c76537-1b6c-3910-c3d4-d21d4e6453a6",
+                    session_id=session_id_value,
                     safe_json=True,
                     web_path=pydantic.HttpUrl(
                         "https://compass.scouts.org.uk/JSon.svc/",
