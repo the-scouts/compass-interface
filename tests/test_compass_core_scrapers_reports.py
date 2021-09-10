51f9e973a7281b44bce48452c0911072edad7dcf
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-10 23:33:23 +0100

    ✅ Add tests for `_report_number`

--- tests/test_compass_core_scrapers_reports.py
+++ tests/test_compass_core_scrapers_reports.py
@@ -6,6 +6,37 @@ from compass.core._scrapers import reports
 
 
 class TestReports:
+    def test_report_number(self):
+        # Given
+        report_type = "Awards Report"
+        hierarchy_level = "District"
+
+        # When
+        out = reports._report_number(report_type, hierarchy_level)
+
+        # Then
+        assert out == 94
+
+    def test_report_number_invalid_type(self):
+        # Given
+        report_type = "Youth Members Report"
+        hierarchy_level = "District"
+
+        # Then
+        with pytest.raises(ci.CompassReportError, match="Youth Members Report is not a valid report type"):
+            # When
+            reports._report_number(report_type, hierarchy_level)
+
+    def test_report_number_invalid_level(self):
+        # Given
+        report_type = "Awards Report"
+        hierarchy_level = "Group"
+
+        # Then
+        with pytest.raises(ci.CompassReportError, match="Requested report does not exist for hierarchy level: Group."):
+            # When
+            reports._report_number(report_type, hierarchy_level)
+
     def test_error_status(self):
         # Given
         transport = httpx.MockTransport(lambda _: httpx.Response(500))
