047507b8e60f5d0b37520f10633db51e6e0d2e21
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-18 01:35:43 +0100

    🔥 Remove redis from `routes.authentication`

--- compass/api/routes/authentication.py
+++ compass/api/routes/authentication.py
@@ -12,11 +12,10 @@ router = fastapi.APIRouter()
 
 
 @router.post("/", response_model=auth.Token)
-async def login_for_access_token(request: requests.Request, form_data: auth.OAuth2Details = auth.Form(...)) -> auth.Token:
+async def login_for_access_token(form_data: auth.OAuth2Details = auth.Form(...)) -> auth.Token:
     logger.debug(f"Requested token endpoint with form data: {form_data.__dict__}")
     try:
-        store = request.app.state.redis
-        access_token = await create_token(form_data.username, form_data.password, form_data.role, form_data.location, store)
+        access_token = await create_token(form_data.username, form_data.password, form_data.role, form_data.location)
     except exceptions.HTTPException as err:
         raise err
     except Exception:
