9ad1e1ef07f669903de9d1afabaa2e617a19334d
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-18 01:13:02 +0100

    ✨ Add (semi-)auto update script for Compass JS files

---
+++ update_js.py
@@ -0,0 +1,50 @@
+import asyncio
+from pathlib import Path
+
+import httpx
+
+BASE_URL = "https://compass.scouts.org.uk/Scripts_v4.07/JS"
+
+files = [
+    "adultjoining",
+    "assignnewrole",
+    "Dates",
+    "editprofile",
+    "extenders",
+    "Grids",
+    "hierarchy",
+    "jQuery191",
+    "master",
+    "memberprofile",
+    "membersearch",
+    "membertraining",
+    "Menu",
+    "neworgentity",
+    "newpermit",
+    "newrole",
+    "newsection",
+    "Popup",
+    "QAS",
+    "reports",
+    "roles",
+    "Scouts",
+    "scoutsportal",
+    "searchresults",
+    "settings",
+    "traininghours",
+    "trainingogl",
+    "updatetraining",
+]
+
+
+async def main() -> list[str]:
+    async with httpx.AsyncClient() as client:
+        tasks = [client.get(f"{BASE_URL}/{filename}.js") for filename in files]
+        responses = await asyncio.gather(*tasks, return_exceptions=True)
+    return [resp.content for resp in responses]
+
+
+if __name__ == '__main__':
+    files_contents = asyncio.run(main())
+    for i, filename in enumerate(files):
+        Path(f"js/{filename}.js").write_bytes(files_contents[i].decode("utf-8-sig").encode("utf-8"))
