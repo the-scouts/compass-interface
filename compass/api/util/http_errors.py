243044b90c33992c4d83b2b8b6a5437649923f20
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-19 05:32:03 +0100

    📝 Error code categories

--- compass/api/util/http_errors.py
+++ compass/api/util/http_errors.py
@@ -51,6 +51,7 @@ class ErrorHandling:
 
 
 api_errors_registry: dict[str, tuple[int, str, dict[str, str] | None]] = {
+    # authentication
     "A1": (status.HTTP_500_INTERNAL_SERVER_ERROR, "Authentication error!", None),
     "A10": (status.HTTP_401_UNAUTHORIZED, "Incorrect username or password!", {"WWW-Authenticate": "Bearer"}),
     "A20": (status.HTTP_401_UNAUTHORIZED, "Could not validate credentials", {"WWW-Authenticate": "Bearer"}),
@@ -60,11 +61,15 @@ api_errors_registry: dict[str, tuple[int, str, dict[str, str] | None]] = {
     "A24": (status.HTTP_401_UNAUTHORIZED, "Could not validate credentials", {"WWW-Authenticate": "Bearer"}),
     "A25": (status.HTTP_401_UNAUTHORIZED, "Could not validate credentials", {"WWW-Authenticate": "Bearer"}),
     "A26": (status.HTTP_401_UNAUTHORIZED, "Your token is malformed! Please get a new token.", {"WWW-Authenticate": "Bearer"}),
+    # authorisation
     "A30": (status.HTTP_403_FORBIDDEN, "Your current role does not have permission to do this!", None),
     "A31": (status.HTTP_403_FORBIDDEN, "Your current role does not have permission to access details for the requested member!", None),
     "A32": (status.HTTP_403_FORBIDDEN, "Your current role does not have permission for the requested unit!", None),
+    # remote
     "R1": (status.HTTP_503_SERVICE_UNAVAILABLE, "The request to the Compass server failed!", None),
+    # hierarchy
     "H10": (status.HTTP_404_NOT_FOUND, "Requested unit ID was not found!", None),
+    # other / general
     "Z0": (status.HTTP_500_INTERNAL_SERVER_ERROR, "API Error (Core)! Please contact Adam.", None),
     "Z1": (status.HTTP_500_INTERNAL_SERVER_ERROR, "Server panic (Library)! Please contact Adam.", None),
     "Z2": (status.HTTP_500_INTERNAL_SERVER_ERROR, "Server panic (Error Handling)! Please contact Adam.", None),
