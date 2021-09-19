efc8089b2aa67873b72c711dafe5bc206022b5d5
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-19 06:25:11 +0100

    ♻️ Remove explicit Nones for clarity

--- compass/api/util/http_errors.py
+++ compass/api/util/http_errors.py
@@ -46,7 +46,7 @@ class ErrorHandling:
 
 # authentication
 _auth_headers = {"WWW-Authenticate": "Bearer"}
-A1 = _HTTPException("A1", status.HTTP_500_INTERNAL_SERVER_ERROR, "Authentication error!", None)
+A1 = _HTTPException("A1", status.HTTP_500_INTERNAL_SERVER_ERROR, "Authentication error!")
 A10 = _HTTPException("A10", status.HTTP_401_UNAUTHORIZED, "Incorrect username or password!", _auth_headers)
 A20 = _HTTPException("A20", status.HTTP_401_UNAUTHORIZED, "Incorrect username or password!", _auth_headers)
 A21 = _HTTPException("A21", status.HTTP_401_UNAUTHORIZED, "Incorrect username or password!", _auth_headers)
@@ -57,22 +57,21 @@ A25 = _HTTPException("A25", status.HTTP_401_UNAUTHORIZED, "Incorrect username or
 A26 = _HTTPException("A26", status.HTTP_401_UNAUTHORIZED, "Your token is malformed! Please get a new token.", _auth_headers)
 
 # authorisation
-A30 = _HTTPException("A30", status.HTTP_403_FORBIDDEN, "Your current role does not have permission to do this!", None)
+A30 = _HTTPException("A30", status.HTTP_403_FORBIDDEN, "Your current role does not have permission to do this!")
 A31 = _HTTPException(
     "A31",
     status.HTTP_403_FORBIDDEN,
     "Your current role does not have permission to access details for the requested member!",
-    None
 )
-A32 = _HTTPException("A32", status.HTTP_403_FORBIDDEN, "Your current role does not have permission for the requested unit!", None)
+A32 = _HTTPException("A32", status.HTTP_403_FORBIDDEN, "Your current role does not have permission for the requested unit!")
 
 # remote
-R1 = _HTTPException("R1", status.HTTP_503_SERVICE_UNAVAILABLE, "The request to the Compass server failed!", None)
+R1 = _HTTPException("R1", status.HTTP_503_SERVICE_UNAVAILABLE, "The request to the Compass server failed!")
 
 # hierarchy
-H10 = _HTTPException("H10", status.HTTP_404_NOT_FOUND, "Requested unit ID was not found!", None)
+H10 = _HTTPException("H10", status.HTTP_404_NOT_FOUND, "Requested unit ID was not found!")
 
 # other / general
-Z0 = _HTTPException("Z0", status.HTTP_500_INTERNAL_SERVER_ERROR, "API Error (Core)! Please contact Adam.", None)
-Z1 = _HTTPException("Z1", status.HTTP_500_INTERNAL_SERVER_ERROR, "Server panic (Library)! Please contact Adam.", None)
-Z2 = _HTTPException("Z2", status.HTTP_500_INTERNAL_SERVER_ERROR, "Server panic (Error Handling)! Please contact Adam.", None)
+Z0 = _HTTPException("Z0", status.HTTP_500_INTERNAL_SERVER_ERROR, "API Error (Core)! Please contact Adam.")
+Z1 = _HTTPException("Z1", status.HTTP_500_INTERNAL_SERVER_ERROR, "Server panic (Library)! Please contact Adam.")
+Z2 = _HTTPException("Z2", status.HTTP_500_INTERNAL_SERVER_ERROR, "Server panic (Error Handling)! Please contact Adam.")
