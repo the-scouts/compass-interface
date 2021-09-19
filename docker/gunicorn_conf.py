b48c07b7ccefe72bb5920f789fe97418c33fc38e
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-19 04:31:13 +0100

    🔧 `int()` can take ints

--- docker/gunicorn_conf.py
+++ docker/gunicorn_conf.py
@@ -6,12 +6,12 @@ port = getenv("PORT", "80")
 
 # Gunicorn config variables
 loglevel = getenv("LOG_LEVEL", "debug")
-workers = int(getenv("WEB_CONCURRENCY", "0")) or max(int(getenv("WORKERS_PER_CORE", "1")) * (cpu_count() or 0), 2)
+workers = int(getenv("WEB_CONCURRENCY", 0)) or max(int(getenv("WORKERS_PER_CORE", 1)) * (cpu_count() or 0), 2)
 bind = getenv("BIND", f"{host}:{port}")
 errorlog = getenv("ERROR_LOG", "-") or None
 worker_tmp_dir = "/dev/shm"
 accesslog = getenv("ACCESS_LOG", "-") or None
-graceful_timeout = int(getenv("GRACEFUL_TIMEOUT", "120"))
-timeout = int(getenv("TIMEOUT", "120"))
-keepalive = int(getenv("KEEP_ALIVE", "5"))
+graceful_timeout = int(getenv("GRACEFUL_TIMEOUT", 120))
+timeout = int(getenv("TIMEOUT", 120))
+keepalive = int(getenv("KEEP_ALIVE", 5))
 worker_class = getenv("WORKER_CLASS", "uvicorn.workers.UvicornWorker")
