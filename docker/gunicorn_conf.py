eda625915c218e963ee9197559ff4c177eab6f7d
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-09-19 04:28:54 +0100

    🔥 Remove printing (gunicorn automatically does this)

--- docker/gunicorn_conf.py
+++ docker/gunicorn_conf.py
@@ -1,27 +1,17 @@
-from os import cpu_count as _cores, getenv as _getenv
+from os import cpu_count, getenv
 
 # Non-gunicorn variables
-host = _getenv("HOST", "0.0.0.0")
-port = _getenv("PORT", "80")
-
+host = getenv("HOST", "0.0.0.0")
+port = getenv("PORT", "80")
 
 # Gunicorn config variables
-loglevel = _getenv("LOG_LEVEL", "debug")
-workers = int(_getenv("WEB_CONCURRENCY", "0")) or max(int(_getenv("WORKERS_PER_CORE", "1")) * (_cores() or 0), 2)
-bind = _getenv("BIND", f"{host}:{port}")
-errorlog = _getenv("ERROR_LOG", "-") or None
+loglevel = getenv("LOG_LEVEL", "debug")
+workers = int(getenv("WEB_CONCURRENCY", "0")) or max(int(getenv("WORKERS_PER_CORE", "1")) * (cpu_count() or 0), 2)
+bind = getenv("BIND", f"{host}:{port}")
+errorlog = getenv("ERROR_LOG", "-") or None
 worker_tmp_dir = "/dev/shm"
-accesslog = _getenv("ACCESS_LOG", "-") or None
-graceful_timeout = int(_getenv("GRACEFUL_TIMEOUT", "120"))
-timeout = int(_getenv("TIMEOUT", "120"))
-keepalive = int(_getenv("KEEP_ALIVE", "5"))
-worker_class = _getenv("WORKER_CLASS", "uvicorn.workers.UvicornWorker")
-
-# Print out all the config variables
-print("GUNICORN CONFIGURATION:")
-_padding = max(len(name) for name in locals() if not name.startswith("_")) + 1
-_name = _value = None  # pre-define, else get "RuntimeError: dictionary changed size during iteration"
-for _name, _value in locals().items():
-    if not _name.startswith("_"):
-        _name = _name + ":"
-        print(f"{_name:<{_padding}} {_value}")
+accesslog = getenv("ACCESS_LOG", "-") or None
+graceful_timeout = int(getenv("GRACEFUL_TIMEOUT", "120"))
+timeout = int(getenv("TIMEOUT", "120"))
+keepalive = int(getenv("KEEP_ALIVE", "5"))
+worker_class = getenv("WORKER_CLASS", "uvicorn.workers.UvicornWorker")
